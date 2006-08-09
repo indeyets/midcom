0f8a04b926633b0f26a34f1d81bae7f154bbf1da
Henri Bergius <henri.bergius@iki.fi>
2006-08-09 08:09:29 +0000

    Added forgotten agendar handler file
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/src@3805 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ org.openpsa.calendar/handler/agenda.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package org.openpsa.calendar
+ * @author The Midgard Project, http://www.midgard-project.org 
+ * @version $Id: report.php,v 1.2 2006/05/10 16:26:10 rambo Exp $
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * Calendar agenda handler
+ * 
+ * @package org.openpsa.calendar
+ */
+class org_openpsa_calendar_handler_agenda extends midcom_baseclasses_components_handler
+{
+    function org_openpsa_calendar_handler_agenda() 
+    {
+        parent::midcom_baseclasses_components_handler();       
+    }
+
+    function _handler_day($handler_id, $args, &$data)
+    {
+        // Generate start/end timestamps for the day
+        $requested_time = @strtotime($args[0]);
+        if (!$requested_time)
+        {
+            return false;
+        }
+        
+        // Use calendarwidget for time calculations
+        $this->_request_data['calendar'] = new org_openpsa_calendarwidget(date('Y', $requested_time), date('m', $requested_time), date('d', $requested_time));
+        $this->_request_data['calendar']->type = ORG_OPENPSA_CALENDARWIDGET_DAY;
+        
+        $from = $this->_request_data['calendar']->get_day_start();
+        $to = $this->_request_data['calendar']->get_day_end();
+
+        // List user's event memberships
+        $qb = midcom_db_eventmember::new_query_builder();
+        $qb->add_constraint('uid', '=', $_MIDGARD['user']);        
+        
+        // Find all events that occur during [$from, $end]
+        $qb->begin_group("OR");
+            // The event begins during [$from, $to]
+            $qb->begin_group("AND");
+                $qb->add_constraint("eid.start", ">=", $from);
+                $qb->add_constraint("eid.start", "<=", $to);
+            $qb->end_group();
+            // The event begins before and ends after [$from, $to]
+            $qb->begin_group("AND");
+                $qb->add_constraint("eid.start", "<=", $from);
+                $qb->add_constraint("eid.end", ">=", $to);
+            $qb->end_group();
+            // The event ends during [$from, $to]
+            $qb->begin_group("AND");
+                $qb->add_constraint("eid.end", ">=", $from);
+                $qb->add_constraint("eid.end", "<=", $to);
+            $qb->end_group();
+        $qb->end_group();
+        
+        $eventmembers = $qb->execute();
+        $this->_request_data['events'] = array();
+        foreach ($eventmembers as $eventmember)
+        {
+            $this->_request_data['events'][] = new org_openpsa_calendar_event($eventmember->eid);
+        }
+
+        return true;
+    }
+    
+    function _show_day($handler_id, &$data)
+    {
+        midcom_show_style('show-day-header');
+        
+        foreach ($this->_request_data['events'] as $event)
+        {
+            $this->_request_data['event'] = $event;
+            midcom_show_style('show-day-item');
+        }
+        
+        midcom_show_style('show-day-footer');
+    }    
+}
+?>
