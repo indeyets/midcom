4a5e6deed52b61bb56d3d3b5d326b4ea93d91749
Henri Bergius <henri.bergius@iki.fi>
2006-08-16 16:48:49 +0000

    Localization work
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/src@3838 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.sales/style/_dm2_composite_deliverable_header.php
+++ org.openpsa.sales/style/_dm2_composite_deliverable_header.php
@@ -10,7 +10,7 @@ $view_data =& $_MIDCOM->get_custom_context_data('midcom_helper_datamanager2_widg
             <th><?php echo $data['l10n']->get('cost'); ?></th>
             <th><?php echo $data['l10n']->get('units'); ?></th>
             <th><?php echo $data['l10n']->get('total'); ?></th>
-            <th><?php echo $data['l10n']->get('cost total'); ?></th>
+            <th><?php echo $data['l10n']->get('total cost'); ?></th>
         </tr>
     </thead>
     <tbody>
