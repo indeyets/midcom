160607c431ac6c84e7a66fff0b3479ed413fda74
Eero af Heurlin <eero.afheurlin@iki.fi>
2006-08-23 17:50:46 +0000

    placeholder/reminder
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/src@3863 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom.helper.datamanager2/exec/universalchooser_create_secret.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Handler for creating shared secret for securing universalchooser searches
+ *
+ * @package midcom.helper.datamanager2
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @version $Id: universalchooser_handler.php 3732 2006-07-21 18:56:43Z rambo $
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+ 
+echo "<p>Not implemented</p>\n";
+ 
+?>
