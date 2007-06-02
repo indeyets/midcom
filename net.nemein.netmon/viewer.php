ddacda76b1950617d09ef25a3216dc8b822549e8
Tarjei Huse <tarjei@nu.no>
2007-06-02 18:49:39 +0000

    #32 Make sure document->component gets set. Forgot this file.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@6138 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.netmon/viewer.php
+++ net.nemein.netmon/viewer.php
@@ -153,6 +153,7 @@ class net_nemein_netmon_viewer extends midcom_baseclasses_components_request
         $document->author = $author->name;
         $document->created = $dm->storage->object->created;
         $document->edited = $dm->storage->object->revised;
+        $document->component = $topic->component;
         $indexer->index($document);
     }
 
