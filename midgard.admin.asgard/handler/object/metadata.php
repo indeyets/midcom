2fd038054e24b0a71f556a3e93bf879e874d748f
Henri Bergius <henri.bergius@iki.fi>
2007-06-02 22:52:14 +0000

    Fix some notices
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@6152 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/handler/object/metadata.php
+++ midgard.admin.asgard/handler/object/metadata.php
@@ -102,6 +102,7 @@ class midgard_admin_asgard_handler_object_metadata extends midcom_baseclasses_co
      */
     function _handler_edit($handler_id, $args, &$data)
     {
+        midgard_admin_asgard_plugin::init_language($handler_id, $args, &$data);    
         $this->_object = $_MIDCOM->dbfactory->get_object_by_guid($args[0]);
         if (! $this->_object)
         {  
@@ -146,7 +147,7 @@ class midgard_admin_asgard_handler_object_metadata extends midcom_baseclasses_co
 
         $this->_prepare_request_data();
         midgard_admin_asgard_plugin::bind_to_object($this->_object, $handler_id, &$data);
-        
+        midgard_admin_asgard_plugin::finish_language($handler_id, &$data);
         return true;
 
     }
