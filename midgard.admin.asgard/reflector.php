a3f8052bcfeb8b338cc6017decc965aa0746c64a
Henri Bergius <henri.bergius@iki.fi>
2007-06-03 00:22:02 +0000

    Remove some common unnecessary parts from type names
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@6160 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/reflector.php
+++ midgard.admin.asgard/reflector.php
@@ -174,7 +174,7 @@ class midgard_admin_asgard_reflector extends midcom_baseclasses_components_purec
                 array_shift($classname_parts);
                 array_shift($classname_parts);
             }
-            $use_label = implode('_', $classname_parts);
+            $use_label = str_replace('openpsa_', '', str_replace('database_', '', implode('_', $classname_parts)));
             $label = $component_l10n->get($use_label);
             if ($use_label == $label)
             {
