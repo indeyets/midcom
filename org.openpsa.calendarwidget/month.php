c433971a0b7373e9bd70358086426073bd580490
Oskari Kokko <oskari.kokko@iki.fi>
2007-07-03 13:32:01 +0000

    Oops, replaced one line too much in forwardport
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11069 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.calendarwidget/month.php
+++ org.openpsa.calendarwidget/month.php
@@ -866,6 +866,7 @@ class org_openpsa_calendarwidget_month
         for ($i = 1; $i <= 7; $i++)
         {
             // Draw the day cell
+            $this->_draw_day($this->_parser);
             $next_day = mktime(0, 0, 0, date('m',$this->_parser), date('d',$this->_parser) + 1, date('Y',$this->_parser));
             // Add one full day to parser timestamp
             $this->_parser = $next_day;
