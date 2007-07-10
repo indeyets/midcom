8309f1dbdb6de5109c4fc839bb57aea37140e2f5
Henri Bergius <henri.bergius@iki.fi>
2007-07-10 10:02:45 +0000

    Support non-DBA objects
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11143 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.datamanager2/type/rcsmessage.php
+++ midcom.helper.datamanager2/type/rcsmessage.php
@@ -25,12 +25,20 @@ class midcom_helper_datamanager2_type_rcsmessage extends midcom_helper_datamanag
 
     function convert_from_storage ($source)
     {
-        $this->value = $this->storage->object->get_rcs_message();
+        if (method_exists($this->storage->object, 'get_rcs_message'))
+        {    
+            $this->value = $this->storage->object->get_rcs_message();
+        }
+        // Nullstorage doesn't have RCS
     }
 
     function convert_to_storage()
     {
-        $this->storage->object->set_rcs_message($this->value);
+        if (method_exists($this->storage->object, 'set_rcs_message'))
+        {
+            $this->storage->object->set_rcs_message($this->value);
+        }
+        // Nullstorage doesn't have RCS
     }
 
     function convert_from_csv ($source)
