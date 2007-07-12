f5771e5e1d7141a85ebd850efea4bf4b0cd0c1ab
Marcin Soltysiak <olga@olga.pl>
2007-07-12 21:48:02 +0000

    cleanup
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11200 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.datamanager2/widget/universalchooser.php
+++ midcom.helper.datamanager2/widget/universalchooser.php
@@ -275,7 +275,7 @@ class midcom_helper_datamanager2_widget_universalchooser extends midcom_helper_d
         if (!is_object($object))
         {
             // Could not object, or got wrong type of object
-            debug_add("Could not get object for key: {$key}", MIDCOM_LOG_ERROR);
+            debug_add("Could not get object for key: {$key}", MIDCOM_LOG_WARN);
             debug_pop();
             return false;
         }
@@ -294,7 +294,7 @@ class midcom_helper_datamanager2_widget_universalchooser extends midcom_helper_d
         {
         	$titlefield =& $this->titlefield;
         }
-debug_add("Pole: $titlefield",MIDCOM_LOG_ERROR);
+
         $value = $object->$titlefield;
         debug_pop();
         return $value;
