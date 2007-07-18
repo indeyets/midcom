e5487fab0daabf6c9f7dd7e08bfac992ba5db1c7
Juhana Hirvilahti <ihirvila@cc.hut.fi>
2007-07-18 10:26:15 +0000

    A minor fix to the favourites listing
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11268 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/style/show_index_item.php
+++ net.nemein.favourites/style/show_index_item.php
@@ -2,11 +2,11 @@
 //$data =& $_MIDCOM->get_custom_context_data('request_data');
 
 $favourite_object = $data['favourite_object'];
-//print_r($favourite_object);
+
 ?>
 
   <li class="net_nemein_favourites_list_item net_nemein_favourites_type_<?php echo $favourite_object->objectType; ?>" id="<?php echo $favourite_object->objectGuid; ?>" >
-    <a href="/simplegray/midcom-permalink-<?php echo $favourite_object->objectGuid; ?>" class="net_nemein_favourites_list_item_link">
+    <a href="<?php echo $_MIDGARD['prefix']; ?>/midcom-permalink-<?php echo $favourite_object->objectGuid; ?>" class="net_nemein_favourites_list_item_link">
     <?php echo $favourite_object->objectTitle; ?></a> <?php echo "Type: " . $favourite_object->objectType; ?>
   </li>
 
