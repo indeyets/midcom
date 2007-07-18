b27c5ccccabc9e26aacd80085977598a4692907a
Marcin Soltysiak <olga@olga.pl>
2007-07-18 10:18:00 +0000

    Fixed coordinates representation that could be broken by locale settings
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11267 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.routamc.positioning/map.php
+++ org.routamc.positioning/map.php
@@ -201,7 +201,7 @@ class org_routamc_positioning_map extends midcom_baseclasses_components_purecode
             $marker_instance = $this->create_js_marker($marker);
             echo "mapstraction_{$this->id}.addMarker({$marker_instance});\n";
         }
-        
+        echo "mapstraction_{$this->id}.addSmallControls();\n";
         echo "mapstraction_{$this->id}.autoCenterAndZoom();\n";
         
         echo "</script>\n";
@@ -214,8 +214,12 @@ class org_routamc_positioning_map extends midcom_baseclasses_components_purecode
     {
         static $i = 0;
         $i++;
-        
-        echo "var marker_{$i} = new Marker(new LatLonPoint({$marker['coordinates']['latitude']}, {$marker['coordinates']['longitude']}))\n";
+
+		// Just in case.. cast lat/lon to 'dot' delimited numbers
+		$lat = number_format($marker['coordinates']['latitude'],6);
+		$lon = number_format($marker['coordinates']['longitude'],6);
+
+        echo "var marker_{$i} = new Marker(new LatLonPoint({$lat}, {$lon}))\n";
         
         echo "marker_{$i}.setLabel('{$marker['title']}');\n";
         
