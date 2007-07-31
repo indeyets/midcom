9732827108ec9f59fbc28df607f0cbb9ee797595
Henri Bergius <henri.bergius@iki.fi>
2007-07-31 10:26:16 +0000

    Fix constraint
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11437 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/handler/view.php
+++ net.nemein.favourites/handler/view.php
@@ -7,8 +7,6 @@
  */
 class net_nemein_favourites_handler_view extends midcom_baseclasses_components_handler
 {
-    var $_content_topic = null;
-
     var $_favourite_objects = Array();
 
     /**
@@ -19,17 +17,12 @@ class net_nemein_favourites_handler_view extends midcom_baseclasses_components_h
         parent::midcom_baseclasses_components_handler();
     }
 
-    function _on_initialize()
-    {
-        $this->_content_topic =& $this->_request_data['content_topic'];
-    }
-
     function _handler_view($handler_id, $args, &$data)
     {
         // Getting favourite objects for the current user
-	$qb = net_nemein_favourites_favourite_dba::new_query_builder();
-	$qb->metadata_creator = $_MIDGARD['user'];
-	$qb->add_order('metadata.created', 'DESC');
+    	$qb = net_nemein_favourites_favourite_dba::new_query_builder();
+    	$qb->add_constraint('metadata.creator', '=', $_MIDGARD['user']);
+    	$qb->add_order('metadata.created', 'DESC');
 
         $this->_favourite_objects = $qb->execute();
   
@@ -39,15 +32,15 @@ class net_nemein_favourites_handler_view extends midcom_baseclasses_components_h
     function _show_view($handler_id, &$data) 
     {
         midcom_show_style('show_index_header');
-        $this->_request_data['favourite_object'] = null;
+        $data['favourite_object'] = null;
 
         foreach ($this->_favourite_objects as $favourite_object)
         {
-	    $this->_request_data['favourite_object'] = $favourite_object;
+    	    $data['favourite_object'] = $favourite_object;
             midcom_show_style('show_index_item');
         }
 
-	midcom_show_style('show_index_footer');
+    	midcom_show_style('show_index_footer');
     }
     
 }
