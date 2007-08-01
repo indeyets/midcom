5aea86638a5fd269913d10959faba7ff3846647d
Juhana Hirvilahti <ihirvila@cc.hut.fi>
2007-08-01 11:04:36 +0000

    Added mgdschemas
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11452 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ net.nehmer.account/config/mgdschema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE net_nehmer_accounts_invites_invite (
+  id int(11) NOT NULL auto_increment,
+  hash varchar(255) NOT NULL default '',
+  email varchar(255) NOT NULL default '',
+  buddy varchar(255) NOT NULL default '',
+  # other fields/indexes as ALTER TABLE statements
+  PRIMARY KEY  (id),
+  KEY net_nehmer_accounts_invites_invite_idx ('hash'(30)),
+  KEY net_nehmer_accounts_invites_invite_idx ('email'(30)),
+  KEY net_nehmer_accounts_invites_invite_idx (`buddy`(30)),
+);
