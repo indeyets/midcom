a0df39f02dd2d1d38e69a6bd5abede3ccf5cd5d8
Henri Bergius <henri.bergius@iki.fi>
2007-08-13 14:21:19 +0000

    Show number of "diggs"
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11573 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/admin.php
+++ net.nemein.favourites/admin.php
@@ -46,19 +46,23 @@ class net_nemein_favourites_admin extends midcom_baseclasses_components_request_
         $midcom_i18n =& $_MIDCOM->get_service('i18n');
         $l10n =& $midcom_i18n->get_l10n('net.nemein.favourites');
         
+        $qb = net_nemein_favourites_favourite_dba::new_query_builder();
+        $qb->add_constraint('objectGuid', '=', $guid);
+        $total_favs = $qb->count_unchecked();
+        
         // Check if user has already favorited this
         $qb = net_nemein_favourites_favourite_dba::new_query_builder();
         $qb->add_constraint('metadata.creator', '=', $_MIDCOM->auth->user->guid);
         $qb->add_constraint('objectGuid', '=', $guid);
         if ($qb->count_unchecked() > 0)
         {
-            echo "<img src=\"" . MIDCOM_STATIC_URL . "/net.nemein.favourites/favorite.png\" alt=\"" . $l10n->get('favourite') . "\" />\n";
+            echo "<span class=\"net_nemein_favourites\">". sprintf($l10n->get('%d favs'), $total_favs) . " <img src=\"" . MIDCOM_STATIC_URL . "/net.nemein.favourites/favorite.png\" alt=\"" . $l10n->get('favourite') . "\" /></span>\n";
         }
         else
         {
             $url = $node[MIDCOM_NAV_FULLURL];
 
-            echo "<a href=\"{$url}create/{$objectType}/{$guid}.html\" class=\"net_nemein_favourites_create\"><img src=\"" . MIDCOM_STATIC_URL . "/net.nemein.favourites/not-favorite.png\" style=\"border: none;\" alt=\"{$l10n->get('add to favourites')}\" /></a>";
+            echo "<span class=\"net_nemein_favourites\">". sprintf($l10n->get('%d favs'), $total_favs) . "<a href=\"{$url}create/{$objectType}/{$guid}.html\" class=\"net_nemein_favourites_create\"> <img src=\"" . MIDCOM_STATIC_URL . "/net.nemein.favourites/not-favorite.png\" style=\"border: none;\" alt=\"{$l10n->get('add to favourites')}\" /></a></span>";
             
             return true;
         }
