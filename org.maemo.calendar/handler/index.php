9a5489d7fd86e78ad20cf2b6cd1d13c8ebf10e28
Jerry Jalava <jerry.jalava@gmail.com>
2007-08-14 15:48:05 +0000

    some more ie 6 fixing
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11606 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.maemo.calendar/handler/index.php
+++ org.maemo.calendar/handler/index.php
@@ -153,7 +153,7 @@ class org_maemo_calendar_handler_index  extends midcom_baseclasses_components_ha
             ORG_MAEMO_CALENDARWIDGET_DAY => 'day',                              
         );
         
-        $script = 'const APPLICATION_PREFIX = "' . $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX) . '";'."\n";
+        $script = 'var APPLICATION_PREFIX = "' . $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX) . '";'."\n";
         $script .= 'var calendar_config = {'."\n";
         $script .= ' type: ' . $this->_request_data['maemo_calender']->type . ',' ."\n";
         $script .= ' start_hour_x: ' . $scrollTop . ',' ."\n";
