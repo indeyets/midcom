36b23ca486a0405e9e3fa60149226f0e4c117389
Juhana Hirvilahti <ihirvila@cc.hut.fi>
2007-08-28 13:58:23 +0000

    Added config.php file
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11775 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ com.magnettechnologies.contactgrabber/lib/yahoo/config.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+  * Contact Grabber
+  * Version 1.0
+  * Released 9th May, 2007
+  * Author: Magnet Technologies, vishal.kothari@magnettechnologies.com
+  * Credits: Janak Prajapati, Pravin Shukla, Tapan Moharana 
+  * Copyright (C) 2007
+
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation; either version 2
+  * of the License, or (at your option) any later version.
+
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+
+  * You should have received a copy of the GNU General Public License
+  * along with this program; if not, write to the Free Software
+  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  **/
+
+// Write full path to this istallation directory
+// example: '/home/user/public_html/contacts' (linux)
+//         
+//$DIR_PATH = "/home/uploads/public_html/contacts";
+$DIR_PATH = "";
+
+// Write the path of curl installation
+// example: '/usr/local/bin/curl' (linux)
+//       
+//$CURL_PATH = "/usr/local/bin/curl";
+$CURL_PATH = "/usr/bin/curl";
+
+// NOTE: make sure that the 'tmp' folder have write permission.
+
+$ERROR_LOGIN = "<br />Login Error...";
+
+?>
