f030aaebb0b48b1efe5ef3b46a184f6c0d278870
Henri Bergius <henri.bergius@iki.fi>
2007-08-29 12:19:46 +0000

    Clearer documentation on adding favourites
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11808 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/style/show_add_favourite.php
+++ net.nemein.favourites/style/show_add_favourite.php
@@ -4,12 +4,16 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
 
 echo "<h1>{$data['topic']->extra}</h1>";
 
+echo "<p class=\"helptext\">{$data['l10n']->get('you are adding the following item to favourites')}</p>\n";
+
 // Showing a form
 echo "<form method=\"post\">";
-echo "<label class=\"net_nemein_favourites_label\">{$data['l10n']->get("title")}</label>";
-echo "<input type=\"hidden\" name=\"net_nemein_favourites_referer\" value=\"{$data['my_way_back']}\"/>";
-echo "<input type=\"text\" name=\"net_nemein_favourite_title\" value=\"{$data['favourite_title']}\"/> ";
-echo "<input type=\"submit\" name=\"net_nemein_favourite_submit\" value=\"{$data['l10n']->get("submit")}\"/>";
+echo "    <input type=\"hidden\" name=\"net_nemein_favourites_referer\" value=\"{$data['my_way_back']}\" />";
+echo "    <label class=\"net_nemein_favourites_label\"><span>{$data['l10n_midcom']->get('title')}</span>";
+echo "        <input type=\"text\" name=\"net_nemein_favourite_title\" value=\"{$data['favourite_title']}\"/> ";
+echo "    </label>\n";
+echo "<input type=\"submit\" name=\"net_nemein_favourite_submit\" value=\"{$data['l10n']->get('add')}\"/>";
 echo "</form>";
 
+echo "<p><a href=\"{$data['my_way_back']}\">{$data['l10n']->get('return and dont add')}</a></p>\n";
 ?>
