765d9a7c5394575e4ad4ef4fb013a9d82991af23
Henri Bergius <henri.bergius@iki.fi>
2007-08-29 11:58:04 +0000

    Show link also for anonymous if so requested
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11804 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/admin.php
+++ net.nemein.favourites/admin.php
@@ -20,7 +20,7 @@ class net_nemein_favourites_admin extends midcom_baseclasses_components_request_
         return true;
     }
 
-    function render_add_link($objectType, $guid, $url = '')
+    function render_add_link($objectType, $guid, $url = '', $link_for_anonymous = true)
     {
         if (   empty($objectType) 
             || empty($guid))
@@ -49,7 +49,8 @@ class net_nemein_favourites_admin extends midcom_baseclasses_components_request_
         $qb->add_constraint('objectGuid', '=', $guid);
         $total_favs = $qb->count_unchecked();
         
-        if (!$_MIDCOM->auth->user)
+        if (   !$_MIDCOM->auth->user
+            && !$link_for_anonymous)
         {
             echo "<span class=\"net_nemein_favourites\">". sprintf($l10n->get('%d favs'), $total_favs) . "</span>\n";
             return true;
