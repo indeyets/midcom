4bb3e201126785554f9b36638b6e168a5803e6b2
Juhana Hirvilahti <ihirvila@cc.hut.fi>
2007-08-31 09:21:46 +0000

    Moved static functions from admin to viewer
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11943 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/viewer.php
+++ net.nemein.favourites/viewer.php
@@ -46,6 +46,64 @@ class net_nemein_favourites_viewer extends midcom_baseclasses_components_request
 
 	return true;
     }
+    
+    function get_add_link($objectType, $guid, $url = '', $link_for_anonymous = true)
+    {
+        if (   empty($objectType) 
+            || empty($guid))
+        {
+            return false;
+        }
+        
+        if (empty($url))
+        {
+            $node = midcom_helper_find_node_by_component('net.nemein.favourites');
+            if (!empty($node))
+            {
+                $url = $node[MIDCOM_NAV_FULLURL];
+            }
+        }
+        
+        if (empty($url))
+        {
+            return false;
+        }
+        
+        $midcom_i18n =& $_MIDCOM->get_service('i18n');
+        $l10n =& $midcom_i18n->get_l10n('net.nemein.favourites');
+        
+        $qb = net_nemein_favourites_favourite_dba::new_query_builder();
+        $qb->add_constraint('objectGuid', '=', $guid);
+        $total_favs = $qb->count_unchecked();
+        
+        if (   !$_MIDCOM->auth->user
+            && !$link_for_anonymous)
+        {
+            return "<span class=\"net_nemein_favourites\">". sprintf($l10n->get('%d favs'), $total_favs) . "</span>\n";
+        }
+        
+        // Check if user has already favorited this
+        $qb = net_nemein_favourites_favourite_dba::new_query_builder();
+        
+        if ($_MIDCOM->auth->user)
+        {
+            $qb->add_constraint('metadata.creator', '=', $_MIDCOM->auth->user->guid);
+        }
+        
+        $qb->add_constraint('objectGuid', '=', $guid);
+        if (   $_MIDCOM->auth->user
+            && $qb->count_unchecked() > 0)
+        {
+            return "<span class=\"net_nemein_favourites\">". sprintf($l10n->get('%d favs'), $total_favs) . " <img src=\"" . MIDCOM_STATIC_URL . "/net.nemein.favourites/favorite.png\" alt=\"" . $l10n->get('favourite') . "\" /></span>\n";
+        }
+
+        return "<span class=\"net_nemein_favourites\">". sprintf($l10n->get('%d favs'), $total_favs) . "<a href=\"{$url}create/{$objectType}/{$guid}.html\" class=\"net_nemein_favourites_create\"> <img src=\"" . MIDCOM_STATIC_URL . "/net.nemein.favourites/not-favorite.png\" style=\"border: none;\" alt=\"{$l10n->get('add to favourites')}\" /></a></span>";
+    }
+
+    function render_add_link($objectType, $guid, $url = '', $link_for_anonymous = true)
+    {
+        echo net_nemein_favourites_admin::get_add_link($objectType, $guid, $url, $link_for_anonymous);
+    }
 }
 
 ?>
