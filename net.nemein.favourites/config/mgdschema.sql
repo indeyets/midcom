18f3480ac69001442f8b7abd80b1307447ec994b
Juhana Hirvilahti <ihirvila@cc.hut.fi>
2007-08-31 08:40:18 +0000

    Fixed ' to `
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@11937 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/config/mgdschema.sql
+++ net.nemein.favourites/config/mgdschema.sql
@@ -7,8 +7,8 @@ CREATE TABLE net_nemein_favourites_favourite (
   sitegroup int(11) NOT NULL default '0',
   guid varchar(80) NOT NULL default '',
   PRIMARY KEY (id),
-  KEY net_nemein_favourites_objectType_idx(«objectType«(30)),
-  KEY net_nemein_favourites_objectGuid_idx(«objectGuid«(30)),
-  KEY net_nemein_favourites_objectTitle_idx(«objectTitle«(30))
+  KEY net_nemein_favourites_objectType_idx(`objectType`(30)),
+  KEY net_nemein_favourites_objectGuid_idx(`objectGuid`(30)),
+  KEY net_nemein_favourites_objectTitle_idx(`objectTitle`(30))
 );
 
