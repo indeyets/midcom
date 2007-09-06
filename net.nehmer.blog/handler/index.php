cb1768798f7f0ebeee275a93247286d3df6dca8a
Henri Bergius <henri.bergius@iki.fi>
2007-09-06 10:07:02 +0000

    Work around MultiLang "wrong language" bug, refs #129
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12032 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nehmer.blog/handler/index.php
+++ net.nehmer.blog/handler/index.php
@@ -130,6 +130,7 @@ class net_nehmer_blog_handler_index extends midcom_baseclasses_components_handle
         }
 
         $qb->add_order('metadata.published', 'DESC');
+        $qb->add_order('lang', 'DESC');
         
         switch ($handler_id)
         {
