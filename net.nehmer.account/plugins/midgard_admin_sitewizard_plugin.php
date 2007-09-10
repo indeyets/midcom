f1f1289d02ba21b222223c96196060f8960025b9
Jerry Jalava <jerry.jalava@gmail.com>
2007-09-10 07:52:15 +0000

    Added configurable redirection
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12090 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nehmer.account/plugins/midgard_admin_sitewizard_plugin.php
+++ net.nehmer.account/plugins/midgard_admin_sitewizard_plugin.php
@@ -40,6 +40,8 @@ class midgard_admin_sitewizard_plugin extends midcom_baseclasses_components_hand
 	    $this->_creation_root_topic_parameters = $this->_request_data['plugin_config']['creation_root_topic_parameters'];
         $this->_creation_root_group_name = $this->_request_data['plugin_config']['creation_root_group_name'];
 
+        $this->_redirect_after_done = $this->_request_data['plugin_config']['redirect_after_done'];
+
         $this->_structure_config_path = $this->_request_data['plugin_config']['structure_config_path'];
 
         if (isset($this->_request_data['plugin_config']['verbose']) && !empty($this->_request_data['plugin_config']['verbose']))
@@ -108,8 +110,7 @@ class midgard_admin_sitewizard_plugin extends midcom_baseclasses_components_hand
             }
 	        elseif ($this->_creation_root_topic_parent_guid != '')
 	        {
-	    
-	        $structure_creator->create_creation_root_topic($this->_creation_root_topic_parent_guid, 
+	            $structure_creator->create_creation_root_topic($this->_creation_root_topic_parent_guid, 
 		        $this->_home_name, $this->_home_title, $this->_creation_root_topic_component, 
 		        $this->_creation_root_topic_parameters, $this->_creation_root_topic_style, true);
 	        }
@@ -121,12 +122,15 @@ class midgard_admin_sitewizard_plugin extends midcom_baseclasses_components_hand
 	        elseif ($this->_cretion_root_group_parent_guid != '')
 	        {
 	            $structure_creator->create_creation_root_group($this->_creation_root_group_guid, 
-	                $this->_creation_root_group_name);
+	            $this->_creation_root_group_name);
 	        }
 	    
 	        $guid = $structure_creator->execute();
-
-	        //$_MIDCOM->relocate($my_way_home);
+            
+            if (! empty($this->_redirect_after_done))
+            {
+                $_MIDCOM->relocate($this->_redirect_after_done);
+            }
 	    }
 	    catch (midgard_admin_sitewizard_exception $e)
 	    {
