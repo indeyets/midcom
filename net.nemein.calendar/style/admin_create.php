5f67d431f761839f785d272c771c516938cc7e93
Henri Bergius <henri.bergius@iki.fi>
2007-09-11 10:23:06 +0000

    Swap node name and view title around
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12120 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.calendar/style/admin_create.php
+++ net.nemein.calendar/style/admin_create.php
@@ -1,9 +1,3 @@
-<?php
-// Available request keys: controller, indexmode, schema, schemadb
-
-//$data =& $_MIDCOM->get_custom_context_data('request_data');
-?>
-
-<h1><?php echo $data['l10n']->get('create event'); ?>: <?php echo $data['topic']->extra; ?></h1>
+<h1><?php echo $data['topic']->extra; ?>: <?php echo $data['l10n']->get('create event'); ?></h1>
 
 <?php $data['controller']->display_form(); ?>
