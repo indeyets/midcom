5ac4478001dd53247e8aafb9e09949f702207361
Jerry Jalava <jerry.jalava@gmail.com>
2007-09-12 11:27:19 +0000

    Trying to fix imagepopup commit
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12167 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom.helper.datamanager2/static/tinymce/plugins/imagepopup/langs/en.js
@@ -0,0 +1,8 @@
+// UK lang variables
+
+/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
+
+tinyMCE.addToLang('',{
+imagepopup_title : 'Insert attachment',
+imagepopup_desc : 'Insert image or attachment'
+});
---
+++ midcom.helper.datamanager2/static/tinymce/plugins/imagepopup/langs/fi.js
@@ -0,0 +1,8 @@
+// UK lang variables
+
+/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
+
+tinyMCE.addToLang('',{
+imagepopup_title : 'Lis&auml;&auml; liitetiedosto',
+imagepopup_desc : 'Lis&auml;&auml; kuva tai liite'
+});
