1eb0075f54b70d65791a33e1e3283b5648d42d6b
Jerry Jalava <jerry.jalava@gmail.com>
2007-09-15 10:41:16 +0000

    Bulletproofing to javascript packing. Added unpacking. Added packer to component packing task. Closes #6
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12302 62f49d1c-1c16-0410-9156-d311ce72abac

--- build/packJavascripts.php
+++ build/packJavascripts.php
@@ -38,6 +38,14 @@ class packJavascripts extends Task
 	
 	private $statistics = null;
 	
+	private $js_src_files = array();
+	
+	private $js_file_names = array();
+	
+	private $no_source = true;
+	
+	private $action = 'pack';
+	
 	/**
 	 * The target directory where the packed files should be saved.
 	 */
@@ -54,6 +62,11 @@ class packJavascripts extends Task
 	public function setPath($str)
 	{
 		$this->path = $str;
+	}
+	
+	public function setAction($str)
+	{
+		$this->action = $str;
 	}
 	
 	/**
@@ -81,8 +94,17 @@ class packJavascripts extends Task
 	{
 	    $this->directory_list_js_files($this->path);
 	    
-	    $this->pack_files();
+	    $this->prepare_files();
 	    
+	    if ($this->action == 'pack')
+	    {
+            $this->pack_files();	        
+	    }
+	    else
+	    {
+            $this->unpack_files();	        
+	    }
+	    	    
 	    $this->project->setProperty($this->returnProperty, $this->statistics);
     }
 	
@@ -103,9 +125,9 @@ class packJavascripts extends Task
 				// Ignore dotfiles
 				continue;
 			}
-			if ($entry == 'CVS' || $entry == '.svn')
+			if ($entry == '.svn')
 			{
-				// Ignore CVS directories
+				// Ignore SVN directories
 				continue;
 			}
 
@@ -113,10 +135,27 @@ class packJavascripts extends Task
 			$path_parts = pathinfo($entry);
 			switch ($path_parts['extension'])
 			{
-				case 'js' :
-					$this->js_files[] = $path . "/" . $entry;
-					//$this->js_packed_files[] = $this->target_dir . "/" . $path_parts['filename'] . ".pack.js";
-					$this->js_packed_files[] = $path . "/" . $path_parts['filename'] . ".pack.js";
+				case 'js':
+				    $clean_filename = str_replace('.src', '', $path_parts['filename']);
+				    $clean_filename = str_replace('.pack', '', $clean_filename);
+				    
+    			    if (! in_array($path_parts['filename'], $this->js_file_names))
+    			    {
+    			        if (   strpos($path_parts['filename'], '.src') === false
+    				        && strpos($path_parts['filename'], '.pack') === false)
+    				    {
+    				        $this->js_file_names[$clean_filename] = $path_parts['filename'];
+    				        $this->js_files[$clean_filename] = $path . "/" . $entry;
+    				    }
+    			    }
+			        if (strpos($path_parts['filename'], '.pack') !== false)
+				    {
+				        $this->js_packed_files[$clean_filename] = $path . "/" . $entry;
+				    }
+			        if (strpos($path_parts['filename'], '.src') !== false)
+				    {
+				        $this->js_src_files[$clean_filename] = $path . "/" . $entry;
+				    }
 					break;
 				default:
 					break;
@@ -131,6 +170,53 @@ class packJavascripts extends Task
 		}
 	}
 	
+	function prepare_files()
+	{
+	    foreach ($this->js_file_names as $clean_name => $file)
+	    {
+	        $old_packed = $this->js_packed_files[$clean_name];
+	        $src_file = $this->js_src_files[$clean_name];
+	        $sel_source = '';
+	        //echo "\nClean name: {$clean_name}, file: {$file}\n";
+	        //echo "\nPacked: {$old_packed}\n";
+	        //echo "\nSource: {$src_file}\n";
+	        
+	        $orig_file_parts = pathinfo($this->js_files[$clean_name]);
+	        $new_packed_file = $orig_file_parts['dirname'] . "/" . $orig_file_parts['filename'] . ".pack.js";
+	        $new_source_file = $orig_file_parts['dirname'] . "/" . $orig_file_parts['filename'] . ".src.js";
+	        	        
+	        if (empty($src_file))
+	        {
+	            //echo "\nWe have no separate source file, assume the original is the source.\n";
+	            $this->no_source = true;
+	            $sel_source = $this->js_files[$clean_name];
+	        }
+	        else
+	        {
+	            //echo "\nWe have source file. Then we also must have either packed file, and/or the original is packed.\n";
+	            $this->no_source = false;
+	            $sel_source = $src_file;
+
+    	        if (! empty($old_packed))
+    	        {
+    	            //echo "\nWe have old packed file. Delete this.\n";
+    	            unlink($old_packed);
+    	        }
+	        }	        
+	        
+	        //echo "\nSelected source file {$sel_source}\n";
+	        
+	        if (! $this->no_source)
+	        {
+	            //Separate source file
+	            //echo "\nRename current packed file {$this->js_files[$clean_name]} to .pack\n";
+	            rename($this->js_files[$clean_name], $new_packed_file);
+	            //echo "\nRename current source file {$sel_source} to {$this->js_files[$clean_name]}\n";
+	            rename($sel_source, $this->js_files[$clean_name]);
+	        }
+	    }
+	}
+	
 	function pack_files()
 	{
 	    $t1 = microtime(true);
@@ -139,8 +225,27 @@ class packJavascripts extends Task
 	    foreach ($this->js_files as $key => $src_file)
 	    {
 	        $src_file_contents = file_get_contents($src_file);
-	        $trgt_file = $this->js_packed_files[$key];
-	        $this->statistics .= "Packing file: {$src_file} to {$trgt_file}... ";
+	        
+	        $src_file_parts = pathinfo($src_file);
+	        $src_new_file = $src_file_parts['dirname'] . "/" . $src_file_parts['filename'] . ".src.js";
+	        $trgt_file = $src_file;
+	        
+	        if (file_exists($src_file))
+	        {
+	            rename($src_file, $src_new_file);
+	            //echo "\nRename source file {$src_file} to {$src_new_file}\n";
+	            $this->statistics .= "\nrename {$src_file} to {$src_new_file}\n";
+            }
+            else
+            {
+                echo "\nFATAL ERROR: Could not find source file {$src_file}\n";
+                continue;
+            }
+            
+            $this->js_src_files[$key] = $src_new_file;
+	        $this->js_packed_files[$key] = $trgt_file;
+	        
+	        $this->statistics .= "Packing file: {$src_new_file} to {$trgt_file}... ";
 
 	        $packer = new JavaScriptPacker($src_file_contents, 'Normal', true, false);
             $packed = $packer->pack();
@@ -153,6 +258,47 @@ class packJavascripts extends Task
         $t2 = microtime(true);
         $time = sprintf('%.4f', ($t2 - $t1) );
         $this->statistics .= "{$file_count} files packed in {$time} seconds. \n";
+	}
+	
+	function unpack_files()
+	{
+	    $t1 = microtime(true);
+	    
+	    $file_count = count($this->js_files);
+	    foreach ($this->js_files as $key => $file)
+	    {
+	        $file_parts = pathinfo($file);
+	        $src_file = $this->js_src_files[$key];
+	        $packed_file = $this->js_packed_files[$key];
+	        $new_src_file = $file;
+	        $new_packed_file = $file_parts['dirname'] . "/" . $file_parts['filename'] . ".pack.js";
+	        
+	        if (file_exists($packed_file))
+	        {
+    	        if (   file_exists($new_packed_file)
+    	            && $packed_file != $new_packed_file)
+    	        {
+    	            //echo "\nWe have old packed file. Delete this.\n";
+    	            unlink($new_packed_file);
+    	            $this->statistics .= "\nunlink {$new_packed_file}\n";
+    	        }
+    	        
+    	        //echo "\nrename {$packed_file} to {$new_packed_file}\n";
+    	        rename($packed_file, $new_packed_file);
+    	        $this->statistics .= "\nrename {$packed_file} to {$new_packed_file}\n";
+            }
+	        
+	        if (file_exists($src_file))
+	        {
+    	        //echo "\nrename {$src_file} to {$new_src_file}\n";
+    	        rename($src_file, $new_src_file);
+    	        $this->statistics .= "\nrename {$src_file} to {$new_src_file}\n";
+            }
+	    }
+	    
+        $t2 = microtime(true);
+        $time = sprintf('%.4f', ($t2 - $t1) );
+        $this->statistics .= "\nunpacked {$file_count} files in {$time} seconds. \n";
 	}
     
 }
