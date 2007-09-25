b7836f6d6eb199c42bbd22a9f6eb759a79202737
Henri Bergius <henri.bergius@iki.fi>
2007-09-25 11:02:39 +0000

    Quick layout fixes and initial XFN work for buddy list front page
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12472 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nehmer.buddylist/style/welcome.php
+++ net.nehmer.buddylist/style/welcome.php
@@ -11,56 +11,80 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
 
 <h2><?php echo $data['topic']->extra; ?></h2>
 
-<?php if ($data['buddies']) { ?>
-
-<form action="&(data['delete_form_action']);" method="post" />
-<table border="0" width="100%">
-<tr>
-   <th width="50%" align="left"><?php $data['l10n_midcom']->show('username'); ?></th>
-   <th align="center"><?php $data['l10n_midcom']->show('online state'); ?></th>
-   <th align="center">&nbsp;</th>
-   <th align="center"><?php $data['l10n_midcom']->show('delete'); ?></th>
-</tr>
-
-<?php
-    foreach ($data['buddies'] as $username => $copy)
-    {
-        $buddy_meta = $data['buddies_meta'][$username];
-        $user =& $data['buddies'][$username];
-?>
+<?php if ($data['buddies'])
+{
+    ?>
+    <form action="&(data['delete_form_action']);" method="post" />
+    <table border="0">
     <tr>
-<?php if ($buddy_meta['view_account_url']) { ?>
-        <td><a href="&(buddy_meta['view_account_url']);">&(username);</a></td>
-<?php } else { ?>
-        <td>&(username);</td>
-<?php } ?>
-        <td align="center"><?php $data['l10n_midcom']->show($buddy_meta['is_online'] ? 'online' : 'offline'); ?></td>
-<?php if ($buddy_meta['new_mail_url']) { ?>
-        <td align="center"><a href="&(buddy_meta['new_mail_url']);"><?php $data['l10n_midcom']->show('write mail'); ?></a></td>
-<?php } else { ?>
-        <td align="center">&nbsp;</td>
-<?php } ?>
-        <td align="center">
-            <input type="checkbox"
-                   name="&(buddy_meta['delete_checkbox_name']);"
-            />
-        </td>
+       <th width="50%" align="left"><?php $data['l10n_midcom']->show('username'); ?></th>
+       <th align="center"><?php $data['l10n_midcom']->show('online state'); ?></th>
+       <th align="center">&nbsp;</th>
+       <th align="center"><?php $data['l10n_midcom']->show('delete'); ?></th>
     </tr>
-    <tr>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td align="center">
-            <input type="submit"
-                   name="&(data['delete_submit_button_name']);"
-                   value="<?php $data['l10n']->show('delete selected'); ?>"
-            />
-        </td>
-    </tr>
-</table>
-</form>
-<?php
-    }
+    <?php
+        foreach ($data['buddies'] as $username => $user)
+        {
+            $buddy_meta = $data['buddies_meta'][$username];
+            if (!$buddy_meta['view_account_url'])
+            {
+                $person = $user->get_storage();
+                $buddy_meta['view_account_url'] = $person->homepage;
+            }
+            
+            ?>
+            <tr>
+            <?php 
+            if ($buddy_meta['view_account_url']) 
+            { 
+                ?>
+                <td><a href="&(buddy_meta['view_account_url']);" rel=\"friend\">&(user.name);</a></td>
+                <?php 
+            } 
+            else
+            {
+                ?>
+                <td>&(user.name);</td>
+                <?php 
+            }
+            ?>
+            <td align="center"><?php $data['l10n_midcom']->show($buddy_meta['is_online'] ? 'online' : 'offline'); ?></td>
+            <?php if ($buddy_meta['new_mail_url'])
+            { 
+                ?>
+                <td align="center"><a href="&(buddy_meta['new_mail_url']);"><?php $data['l10n_midcom']->show('write mail'); ?></a></td>
+                <?php
+            } 
+            else
+            {
+                ?>
+                <td align="center">&nbsp;</td>
+                <?php 
+            } 
+            ?>
+            <td align="center">
+                <input type="checkbox"
+                       name="&(buddy_meta['delete_checkbox_name']);"
+                />
+            </td>
+        </tr>
+        <?php
+        }
+    ?>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td align="center">
+                <input type="submit"
+                       name="&(data['delete_submit_button_name']);"
+                       value="<?php $data['l10n']->show('delete selected'); ?>"
+                />
+            </td>
+        </tr>
+    </table>
+    </form>
+    <?php
 }
 else
 {
