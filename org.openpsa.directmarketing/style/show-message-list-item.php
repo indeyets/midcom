e53a4e4628ed1c26e940abc7c7a82cb1465b9f23
Henri Bergius <henri.bergius@iki.fi>
2007-09-26 12:47:30 +0000

    Use metadata property
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12486 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.directmarketing/style/show-message-list-item.php
+++ org.openpsa.directmarketing/style/show-message-list-item.php
@@ -4,7 +4,7 @@ $nap = new midcom_helper_nav();
 $node = $nap->get_node($nap->get_current_node());
 
 echo "<div class=\"{$data['message_class']}\"><a href=\"{$node[MIDCOM_NAV_FULLURL]}message/{$data['message']->guid}/\">{$view['title']}</a>\n";
-echo "<br />".sprintf($data['l10n']->get('created on %s'), strftime('%x %X', $data['message']->created))."\n";
+echo "<br />".sprintf($data['l10n']->get('created on %s'), strftime('%x %X', $data['message']->metadata->created))."\n";
 
 if ($data['message']->sendStarted)
 {
