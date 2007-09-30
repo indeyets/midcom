e3d5e2d94862615f5eedddfd7417621f481a6b04
Henri Bergius <henri.bergius@iki.fi>
2007-09-30 12:29:19 +0000

    Fix notices when object has not yet been created
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12602 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.imagepopup/style/midcom_helper_imagepopup_navigation.php
+++ midcom.helper.imagepopup/style/midcom_helper_imagepopup_navigation.php
@@ -9,9 +9,11 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX) . "__ais/image
         if ($data['object'])
         {
             echo "<li><a href=\"{$prefix}{$data['schema_name']}/{$data['object']->guid}/\">" . $_MIDCOM->i18n->get_string('page', 'midcom') . "</a></li>";
+            echo "<li class=\"selected\"><a href=\"{$prefix}folder/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
+            echo "<li><a href=\"{$prefix}unified/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
         }
-        echo "<li class=\"selected\"><a href=\"{$prefix}folder/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
-        echo "<li><a href=\"{$prefix}unified/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
+        echo "<li class=\"selected\"><a href=\"{$prefix}folder/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
+        echo "<li><a href=\"{$prefix}unified/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
     }
     else if ($data['list_type'] === 'page')
     {
@@ -21,9 +23,14 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX) . "__ais/image
     }
     else if ($data['list_type'] === 'unified')
     {
-        echo "<li><a href=\"{$prefix}{$data['schema_name']}/{$data['object']->guid}/\">" . $_MIDCOM->i18n->get_string('page', 'midcom') . "</a></li>";
-        echo "<li><a href=\"{$prefix}folder/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
-        echo "<li class=\"selected\"><a href=\"{$prefix}unified/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
+        if ($data['object'])
+        {
+            echo "<li><a href=\"{$prefix}{$data['schema_name']}/{$data['object']->guid}/\">" . $_MIDCOM->i18n->get_string('page', 'midcom') . "</a></li>";
+            echo "<li><a href=\"{$prefix}folder/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
+            echo "<li class=\"selected\"><a href=\"{$prefix}unified/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
+        }
+        echo "<li><a href=\"{$prefix}folder/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
+        echo "<li class=\"selected\"><a href=\"{$prefix}unified/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
     }
 
    ?>
