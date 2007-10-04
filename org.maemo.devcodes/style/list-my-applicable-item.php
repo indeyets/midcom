f8a089fa9117725e688c02c7500a55bdd71ab4d1
Eero af Heurlin <eero.afheurlin@iki.fi>
2007-10-04 05:51:41 +0000

    forward port r12659
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12660 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.maemo.devcodes/style/list-my-applicable-item.php
+++ org.maemo.devcodes/style/list-my-applicable-item.php
@@ -4,4 +4,21 @@ $device =& $data['device'];
 ?>
     <li>
         <a href="&(data['prefix']);device/applyfor/&(device.guid);">&(device.title);</a>
+        <?php
+        if (   !empty($device->end)
+            && $device->end !== '0000-00-00 00:00:00')
+        {
+            $deadline_ts = strtotime($device->end);
+            $deadline_f = strftime('%x', $deadline_ts);
+            $days = floor(($deadline_ts - time()) / (3600*24));
+            if ($days > 0)
+            {
+                echo sprintf($data['l10n']->get('apply before %s'), $deadline_f);
+            }
+            else
+            {
+                echo $data['l10n']->get('apply now');
+            }
+        }
+        ?>
     </li>
