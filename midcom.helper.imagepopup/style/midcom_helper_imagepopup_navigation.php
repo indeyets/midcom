0b708411777067beb4861c9b84897a48d88a46c5
Henri Bergius <henri.bergius@iki.fi>
2007-10-12 12:24:39 +0000

    Fix double navigation in search
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12774 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.imagepopup/style/midcom_helper_imagepopup_navigation.php
+++ midcom.helper.imagepopup/style/midcom_helper_imagepopup_navigation.php
@@ -29,8 +29,11 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX) . "__ais/image
             echo "<li><a href=\"{$prefix}folder/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
             echo "<li class=\"selected\"><a href=\"{$prefix}unified/{$data['schema_name']}/{$data['object']->guid}\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
         }
-        echo "<li><a href=\"{$prefix}folder/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
-        echo "<li class=\"selected\"><a href=\"{$prefix}unified/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
+        else
+        {
+            echo "<li><a href=\"{$prefix}folder/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('folder', 'midcom') . "</a></li>";
+            echo "<li class=\"selected\"><a href=\"{$prefix}unified/{$data['schema_name']}/\">" . $_MIDCOM->i18n->get_string('unified search', 'midcom.helper.imagepopup') . "</a></li>";
+        }
     }
 
    ?>
