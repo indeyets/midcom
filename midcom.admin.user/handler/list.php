785045ba5f5e43d50bc86a91aa9de20ec66f2354
Marcin Soltysiak <olga@olga.pl>
2007-10-12 21:02:57 +0000

    typo
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12790 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.admin.user/handler/list.php
+++ midcom.admin.user/handler/list.php
@@ -88,7 +88,7 @@ class midcom_admin_user_handler_list extends midcom_baseclasses_components_handl
         (
             array
             (
-                MIDCOM_TOOLBAR_URL => $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX)."/midcom-logout-",
+                MIDCOM_TOOLBAR_URL => $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX)."midcom-logout-",
                 MIDCOM_TOOLBAR_LABEL => $_MIDCOM->i18n->get_string('logout','midcom'),
                 MIDCOM_TOOLBAR_ICON => 'stock-icons/16x16/exit.png',
             )
