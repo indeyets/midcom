86d2883ed23b3d072cffd61fd5874e15edbb595b
Marcin Soltysiak <olga@olga.pl>
2007-10-12 16:11:11 +0000

    and here..
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12784 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/handler/welcome.php
+++ midgard.admin.asgard/handler/welcome.php
@@ -61,6 +61,17 @@ class midgard_admin_asgard_handler_welcome extends midcom_baseclasses_components
                 MIDCOM_TOOLBAR_ICON => 'stock-icons/16x16/gohome.png',
             )
         );
+
+        $data['asgard_toolbar']->add_item
+        (
+            array
+            (
+                MIDCOM_TOOLBAR_URL => $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX)."/midcom-logout-",
+                MIDCOM_TOOLBAR_LABEL => $_MIDCOM->i18n->get_string('logout','midcom'),
+                MIDCOM_TOOLBAR_ICON => 'stock-icons/16x16/exit.png',
+            )
+        );
+
         return true;
     }
 
