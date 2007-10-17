e63e2c490bfff874c62094edff43737ae0546eb1
Henri Bergius <henri.bergius@iki.fi>
2007-10-17 14:04:40 +0000

    View listing folders a group has any permissions set for
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12858 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.admin.user/handler/group/permissions.php
+++ midcom.admin.user/handler/group/permissions.php
@@ -12,7 +12,7 @@
  * 
  * @package midcom.admin.user
  */
-class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components_handler
+class midcom_admin_user_handler_group_permissions extends midcom_baseclasses_components_handler
 {
     var $_group = null;
 
@@ -21,7 +21,7 @@ class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components
      * 
      * @access public
      */
-    function midcom_admin_user_handler_group_edit()
+    function midcom_admin_user_handler_group_permissions()
     {
         $this->_component = 'midcom.admin.user';
         parent::midcom_baseclasses_components_handler();
@@ -51,8 +51,14 @@ class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components
     {
         // Populate breadcrumb
         $tmp = Array();
+        $grp = $this->_group;
         
-        $grp = $this->_group;       
+        $tmp[] = Array
+        (
+            MIDCOM_NAV_URL => "__mfa/asgard_midcom.admin.user/group/folders/{$grp->guid}",
+            MIDCOM_NAV_NAME => $_MIDCOM->i18n->get_string('folders', 'midcom.admin.user'),
+        );
+        
         while ($grp)
         {        
             $tmp[] = Array
@@ -71,33 +77,6 @@ class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components
         
         $_MIDCOM->set_custom_context_data('midcom.helper.nav.breadcrumb', $tmp);
     }
-
-    /**
-     * Loads and prepares the schema database.
-     */
-    function _load_schemadb()
-    {
-        $this->_schemadb = midcom_helper_datamanager2_schema::load_database($this->_config->get('schemadb_group'));
-    }
-
-    /**
-     * Internal helper, loads the controller for the current group. Any error triggers a 500.
-     *
-     * @access private
-     */
-    function _load_controller()
-    {
-        $this->_load_schemadb();
-        $this->_controller =& midcom_helper_datamanager2_controller::create('simple');
-        $this->_controller->schemadb =& $this->_schemadb;
-        $this->_controller->set_storage($this->_group, 'default');
-        if (! $this->_controller->initialize())
-        {
-            $_MIDCOM->generate_error(MIDCOM_ERRCRIT, "Failed to initialize a DM2 controller instance for group {$this->_group->id}.");
-            // This will exit.
-        }
-    }
-
     
     /**
      * Handler method for listing style elements for the currently used component topic
@@ -108,7 +87,7 @@ class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components
      * @param mixed $data Data passed to the show method
      * @return boolean Indicating successful request
      */
-    function _handler_edit($handler_id, $args, &$data)
+    function _handler_folders($handler_id, $args, &$data)
     {
         $this->_group = new midcom_db_group($args[0]);
         if (   !$this->_group
@@ -116,43 +95,35 @@ class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components
         {
             return false;
         }
-        $this->_group->require_do('midgard:update');
     
-                
         $data['asgard_toolbar'] = new midcom_helper_toolbar();
-        
-        $this->_load_controller();
-        switch ($this->_controller->process_form())
-        {
-            case 'save':
-                // Show confirmation for the group
-                $_MIDCOM->uimessages->add($this->_l10n->get('midcom.admin.user'), sprintf($this->_l10n->get('group %s saved'), $this->_group->name));
-                $_MIDCOM->relocate("__mfa/asgard_midcom.admin.user/group/edit/{$this->_group->guid}/");
-                // This will exit.
-                
-            case 'cancel':
-                $_MIDCOM->relocate('__mfa/asgard_midcom.admin.user/');
-                // This will exit.
-        }
-        
-
         $data['language_code'] = '';
         midgard_admin_asgard_plugin::bind_to_object($this->_group, $handler_id, &$data);
 
-        $data['view_title'] = sprintf($_MIDCOM->i18n->get_string('edit %s', 'midcom.admin.user'), $this->_group->official);
+        $qb = new midgard_query_builder('midcom_core_privilege_db');
+        $qb->add_constraint('assignee', '=', "group:{$this->_group->guid}");
+        $privileges = $qb->execute();
+        $data['objects'] = array();
+        $data['privileges'] = array();
+        foreach ($privileges as $privilege)
+        {
+            if (!$privilege->objectguid)
+            {
+                // We're only interested in privs applying to objects now, skip
+                continue;
+            }
+            $data['privileges'][] = $privilege->name;
+            if (!isset($data['objects'][$privilege->objectguid]))
+            {
+                $data['objects'][$privilege->objectguid] = array();
+            }
+            $data['objects'][$privilege->objectguid][$privilege->name] = $privilege->value;
+        }
+
+        $data['view_title'] = sprintf($_MIDCOM->i18n->get_string('folders of %s', 'midcom.admin.user'), $this->_group->official);
         $_MIDCOM->set_pagetitle($data['view_title']);
 
         $this->_update_breadcrumb();
-        
-        $data['asgard_toolbar']->add_item
-        (
-            array
-            (
-                MIDCOM_TOOLBAR_URL => "__mfa/asgard_midcom.admin.user/group/folders/{$this->_group->guid}/",
-                MIDCOM_TOOLBAR_LABEL => $_MIDCOM->i18n->get_string('folders', 'midcom.admin.user'),
-                MIDCOM_TOOLBAR_ICON => 'stock-icons/16x16/folder.png',
-            )
-        );
         
         return true;
     }
@@ -164,13 +135,12 @@ class midcom_admin_user_handler_group_edit extends midcom_baseclasses_components
      * @param string $handler_id Name of the used handler
      * @param mixed $data Data passed to the show method
      */
-    function _show_edit($handler_id, &$data)
+    function _show_folders($handler_id, &$data)
     {
         midgard_admin_asgard_plugin::asgard_header();        
 
         $data['group'] =& $this->_group;
-        $data['controller'] =& $this->_controller;
-        midcom_show_style('midcom-admin-user-group-edit');
+        midcom_show_style('midcom-admin-user-group-folders');
         
         midgard_admin_asgard_plugin::asgard_footer();
     }
