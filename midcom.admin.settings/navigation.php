188e0d1447dff4e77904376b006e04a12465a106
Marcin Soltysiak <olga@olga.pl>
2007-10-20 20:30:46 +0000

    navi class
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12920 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom.admin.settings/navigation.php
@@ -0,0 +1,15 @@
+<?php
+
+class midcom_admin_settings_navigation extends midcom_baseclasses_components_navigation
+{
+    /**
+     * Simple constructor, calls base class.
+     */
+    function  midcom_admin_settings_navigation()
+    {
+        parent::midcom_baseclasses_components_navigation();
+    }
+
+}
+
+?>
