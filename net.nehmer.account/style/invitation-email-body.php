554134c0081dc974dd6fe9a929ec96d10ee17b8c
Jerry Jalava <jerry.jalava@gmail.com>
2007-10-22 12:36:23 +0000

    Fixed link address
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@12968 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nehmer.account/style/invitation-email-body.php
+++ net.nehmer.account/style/invitation-email-body.php
@@ -1,7 +1,7 @@
 <?php
 $hash = $data['hash'];
-$prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
-$register_link = $_MIDCOM->get_host_prefix() . "{$prefix}register_invitation/{$hash}/";
+$prefix = substr($_MIDCOM->get_host_prefix(), 0, -1) . $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
+$register_link = "{$prefix}register_invitation/{$hash}/";
 $message = $data['user_message'];
 ?>
 &(message);
