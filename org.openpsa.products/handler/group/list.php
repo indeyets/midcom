66f09a44c8c6260a4e986bb22ac10341b56a28bc
Henri Bergius <henri.bergius@iki.fi>
2007-11-05 08:51:58 +0000

    Backwards compatibility
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13261 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.products/handler/group/list.php
+++ org.openpsa.products/handler/group/list.php
@@ -350,7 +350,7 @@ class org_openpsa_products_handler_group_list  extends midcom_baseclasses_compon
 
         $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
 
-        if (count($data['groups']) == 1) //FIXME: Hack
+        if (count($data['groups']) >= 1) //FIXME: Hack
         {
     	    midcom_show_style('group_header');
 
@@ -398,7 +398,7 @@ class org_openpsa_products_handler_group_list  extends midcom_baseclasses_compon
     	    midcom_show_style('group_footer');
         }
 
-        if (count($data['products']) > 0)
+        elseif (count($data['products']) > 0)
         {
     	    midcom_show_style('group_header');
 
