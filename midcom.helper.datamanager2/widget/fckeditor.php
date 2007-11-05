37b09cb3ccc4f6ee589d06d149e458dc95b84649
Jerry Jalava <jerry.jalava@gmail.com>
2007-11-05 13:03:17 +0000

    Added configuration possibility to fckeditor -widget. Closes #176
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13273 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.datamanager2/widget/fckeditor.php
+++ midcom.helper.datamanager2/widget/fckeditor.php
@@ -24,8 +24,8 @@ require_once('textarea.php');
  *
  * <b>Available configuration options:</b>
  *
- * - All of the textarea baseclass. The height default has been changed to 25, the width default
- *   to 80.
+ * - All of the textarea baseclass. The height default has been changed to 450, the width default
+ *   to 800.
  * - <i>string fck_config_snippet:</i> Indicates the name of the snippet which holds the base
  *   configuration. This is looked up in the DM2 directory in SG-Config. This defaults to
  *   '$GLOBALS['midcom_config']['midcom_sgconfig_basedir']/midcom.helper.datamanager2/fckeditor'.
@@ -46,6 +46,22 @@ require_once('textarea.php');
 class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamanager2_widget_textarea
 {
     /**
+     * Width of the editor.
+     *
+     * @var int
+     * @access public
+     */
+    var $width = null;
+
+    /**
+     * Height of the editor.
+     *
+     * @var int
+     * @access public
+     */
+    var $height = null;
+
+    /**
      * The FCK configuration snippet to use. Argument must be applicable to use with
      * midcom_get_snippet_content.
      *
@@ -58,12 +74,19 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
      *
      * @var string
      */
-    var $local_config = '';
+    var $local_config = null;
+    
+    /**
+     * Local js configuration to be added to the config snippet.
+     *
+     * @var string
+     */
+    var $local_js_config = null;
 
     /**
      * Which theme will the editor use to render itself
      *
-     * @var string : valid values: default, office and silver
+     * @var string : valid values: default, office2003 and silver
      */
     var $theme = null;
     
@@ -72,50 +95,27 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
      *
      * @var string : valid values: Default or Basic
      */
-    var $toolbar_set = "Default";
+    var $toolbar_set = null;
+    var $toolbar_set_content = array();
     
-    /**
-     * Should the midcom image dialog be used instead of the normal?
-     * @var boolean defaults to true.
-     */
-    var $use_midcom_imagedialog = true;
+    var $_static_toolbar_sets = array(
+        'Default',
+        'Basic',
+    );
+    var $_default_toolbar_set_contents = array();
     
-    var $editor;
+    var $_editor;
     
     var $configuration = array();
     
     var $fckeditor_path = null;
-    
-    /**
-     * Adds the external HTML dependencies, both JS and CSS. A static flag prevents
-     * multiple insertions of these dependencies.
-     *
-     * @access private
-     */
-    function _add_external_html_elements()
-    {
-        static $executed = false;
-
-        if ($executed)
-        {
-            return;
-        }
-
-        $executed = true;
-    }
 
     /**
      * This helper will construct the FCKeditor initscript based on the specified configuration.
      */
     function _add_initscript()
     {
-
-        $this->configuration['fckconfig'] = midcom_get_snippet_content_graceful($this->fck_config_snippet);
-        
-        if (! $this->configuration['fckconfig'])
-        {
-            $this->_get_configuration();
-        }
+        $this->_get_configuration();
 		
         $language = $_MIDCOM->i18n->get_current_language();
         // fix to use the correct langcode for norwegian.
@@ -131,53 +131,148 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
      * @access private
      */
     function _get_configuration()
-    {   
-        $valid_sets = array(
-            'Default'   => true,
-            'Basic' => true,
-        );
+    {
+        $this->toolbar_set_contents = $this->_default_toolbar_set_contents;
         
-        if (array_key_exists($this->toolbar_set, $valid_sets))
+        if (! empty($this->local_config))        
+        {
+            $this->configuration['fckconfig'] = $this->local_config;            
+        }
+
+        if (! empty($this->local_js_config))        
+        {
+            $this->configuration['js_config'] = $this->local_js_config;            
+        }
+
+        if (!is_null($this->toolbar_set))
         {
             $this->configuration['toolbar_set'] = $this->toolbar_set;
-        }
-        else
+        }        
+        
+        $this->configuration['width'] = "800px";
+        $this->configuration['height'] = "450px";
+                
+        if (! is_null($this->width))
         {
-            $this->configuration['toolbar_set'] = 'Default';
+            if (is_numeric($this->width))
+            {
+                $this->width = "{$this->width}px";
+            }
+            $this->configuration['width'] = $this->width;
         }
-        
-        if ($this->fck_config_snippet != '') 
+        if (! is_null($this->height))
         {
-            $this->configuration['toolbar_set'] = 'Custom';
-            $this->configuration['toolbar_set_content'] = $this->fck_config_snippet;
+            if (is_numeric($this->height))
+            {
+                $this->height = "{$this->height}px";
+            }
+            $this->configuration['height'] = $this->height;
         }
-
-        $this->height = 450;            
-        $this->width = 800;
         
-        $this->configuration['width'] = $this->width;
-        $this->configuration['height'] = $this->height;
+        if ($this->fck_config_snippet != '')
+        {
+            $data = midcom_get_snippet_content($this->fck_config_snippet);
+            $result = eval ("\$snippet_config = Array ( {$data}\n );");
+            if ($result === false)
+            {
+                $_MIDCOM->generate_error(MIDCOM_ERRCRIT,
+                    "Failed to parse the config file in '{$this->fck_config_snippet}', see above for PHP errors.");
+                // This will exit.
+            }
+
+            if (isset($snippet_config['fckconfig']))
+            {
+                $this->configuration['fckconfig'] = $snippet_config['fckconfig'];
+                if (! empty($this->local_config))
+                {
+                    $this->configuration['fckconfig'] = array_merge($this->configuration['fckconfig'], $this->local_config);            
+                }
+            }
+            
+            if (isset($snippet_config['js_config']))
+            {
+                $this->configuration['js_config'] = $snippet_config['js_config'];
+                if (! empty($this->local_js_config))
+                {
+                    $this->configuration['js_config'] = array_merge($this->configuration['js_config'], $this->local_js_config);            
+                }
+            }
+            
+            if (   isset($snippet_config['toolbar_set'])
+                && is_null($this->toolbar_set))
+            {
+                $this->configuration['toolbar_set'] = $snippet_config['toolbar_set'];
+            }
+            
+            if (   isset($snippet_config['toolbar_set_content'])
+                && !empty($snippet_config['toolbar_set_content']))
+            {
+                $this->toolbar_set_contents = array_merge($this->toolbar_set_content, $snippet_config['toolbar_set_content']);
+            }           
+            
+            if (   isset($snippet_config['width'])
+                && is_null($this->width))
+            {
+                $this->configuration['width'] = $snippet_config['width'];
+            }
+            
+            if (   isset($snippet_config['height'])
+                && is_null($this->height))
+            {
+                $this->configuration['height'] = $snippet_config['height'];
+            }
+        }
         
-        $this->configuration['fckconfig'] = array();
+        if (! empty($this->toolbar_set_content))
+        {
+            $this->toolbar_set_contents = array_merge(
+                $this->toolbar_set_contents,
+                array
+                (
+                    $this->configuration['toolbar_set'] => $this->toolbar_set_content
+                )
+            );            
+        }
+        
+        if (   !in_array($this->toolbar_set, $this->_static_toolbar_sets)
+            && isset($this->toolbar_set_contents[$this->configuration['toolbar_set']]))
+        {
+            $this->configuration['toolbar_set_content'] = $this->toolbar_set_contents[$this->configuration['toolbar_set']];
+        }
+        
+        if (empty($this->configuration['fckconfig']))
+        {
+            $this->configuration['fckconfig'] = array
+            (
+                'SkinPath' => 'editor/skins/office2003/',
+                'DefaultLanguage' => 'en-uk',
+                'UseBROnCarriageReturn' => 'false',
+                'StartupFocus' => 'false',
+            );
+        }
+        
+        $this->configuration['fckconfig']['CustomConfigurationsPath'] = $this->_resolve_custom_config_path();
+        
     }
-
-    /**
-     * Returns the Default configuraiton:
-     *
-     * @return string The default configuration
-     * @access private
-     */
-    function _get_default_configuration()
+    
+    function _resolve_custom_config_path()
     {
-        $this->configuration['toolbar_set'] = 'Default';
-    }
-
-    /**
-     * Returns the "basic" configuration
-     */
-    function _get_basic_configuration ()
-    {
-        $this->configuration['toolbar_set'] = 'Basic';
+        $prefix = $_MIDCOM->get_host_prefix();
+        $config_str = '';
+        
+        $tmp_config = $this->configuration['js_config'];
+        
+        if (isset($this->configuration['toolbar_set_content']))
+        {
+            $tmp_config['toolbar_set'] = $this->configuration['toolbar_set'];
+            $tmp_config['toolbar_content'] = $this->configuration['toolbar_set_content'];
+        }
+        
+        //var_dump($tmp_config);
+        
+        $config_str = base64_encode(serialize($tmp_config));
+        
+        return "{$prefix}midcom-exec-midcom.helper.datamanager2/fckeditor/config.php?config_str={$config_str}";
     }
     
     /**
@@ -187,15 +282,22 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
      * The systemwide defaults for the theme and the fck config snippet will be loaded
      * from the config file at this point.
      *
-     * @todo make overrideable.
      * @access private
      */
     function _on_configuring()
     {
         parent::_on_configuring();
-                
-        $this->theme = $this->_config->get('fck_default_theme');
-        $this->toolbar_set = $this->_config->get('fck_default_toolbar_set');
+        
+        $this->configuration['fckconfig'] = array
+        (
+            'SkinPath' => 'editor/skins/office2003/',
+            'DefaultLanguage' => 'en-uk',
+            'UseBROnCarriageReturn' => 'false',
+            'StartupFocus' => 'false',
+        );
+        
+        $this->_static_toolbar_sets = $this->_config->get('fck_static_toolbar_sets');
+        $this->_default_toolbar_set_contents = $this->_config->get('fck_default_toolbar_set_contents');
         $this->fck_config_snippet = $this->_config->get('fck_default_config_snippet');
         
         $this->fckeditor_path = MIDCOM_ROOT . '/midcom/helper/datamanager2/static/fckeditor/';
@@ -207,15 +309,9 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
      * @return boolean always true
      */
     function _on_initialize ()
-    {
-        if (!$this->toolbar_set)
-        {
-            $this->toolbar_set = 'Default';
-        }
-        
+    {        
         if ($this->_initialize_dependencies)
         {
-            $this->_add_external_html_elements();
             $this->_add_initscript();
         }
 
@@ -235,7 +331,6 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
         
         if (!$this->_initialize_dependencies)
         {
-            $this->_add_external_html_elements();
             $this->_add_initscript();
         }
         
@@ -245,14 +340,14 @@ class midcom_helper_datamanager2_widget_fckeditor extends midcom_helper_datamana
             'id'    => "{$this->_namespace}{$this->name}",
         );
 
-        $this->editor = $this->_form->addElement(
+        $this->_editor = $this->_form->addElement(
             'fckeditor',
             $this->name,
             $this->_translate($this->_field['title']),
             $attributes
         );
 
-        $this->editor->setFCKProps(
+        $this->_editor->setFCKProps(
             $this->configuration['basepath'],
             $this->configuration['toolbar_set'],
             $this->configuration['width'],
