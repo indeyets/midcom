a358c7b06e37beafe97a0553728fb3d431865c01
Arttu Manninen <arttu@kaktus.cc>
2007-11-21 11:06:31 +0000

    User interface improvements with jQuery
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13530 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.admin.user/static/jquery.midcom_admin_user.js
+++ midcom.admin.user/static/jquery.midcom_admin_user.js
@@ -18,6 +18,9 @@ jQuery.fn.check_all = function(target)
         {
             $j(this).attr('checked', '');
         }
+        
+        // Trigger the onChange event of the input
+        $j(this).change();
     });
 }
 
@@ -39,7 +42,45 @@ jQuery.fn.invert_selection = function(target)
         {
             $j(this).attr('checked', 'checked');
         }
+        
+        // Trigger the onChange event of the input
+        $j(this).change();
     });
     
     $j(this).attr('checked', '');
 }
+
+$j(document).ready(function()
+{
+    $j('#midcom_admin_user_batch_process tbody tr').find('td:first').addClass('first');
+    $j('#midcom_admin_user_batch_process tbody tr').find('td:last').addClass('last');
+    
+    $j("#midcom_admin_user_batch_process tbody input[@type='checkbox']").each(function(i)
+    {
+        $j(this).change(function()
+        {
+            var object = this.parentNode;
+            var n = 0;
+            
+            while (!object.tagName.match(/tr/i))
+            {
+                object = object.parentNode;
+                
+                // Protect against infinite loops
+                if (n > 20)
+                {
+                    return;
+                }
+            }
+            
+            if ($j(this).attr('checked'))
+            {
+                $j(object).addClass('row_selected');
+            }
+            else
+            {
+                $j(object).removeClass('row_selected');
+            }
+        });
+    });
+});
