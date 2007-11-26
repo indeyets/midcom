61b10976c92817c388e5b09b195e7abf22aba9c2
Arttu Manninen <arttu@kaktus.cc>
2007-11-26 14:24:10 +0000

    Configurable navigation.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13654 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/navigation.php
+++ midgard.admin.asgard/navigation.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * @package midgard.admin.asgard
+ * @author The Midgard Project, http://www.midgard-project.org 
+ * @version $Id: acl_editor.php 5538 2007-03-20 13:22:41Z rambo $
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * Navigation class for Asgard
+ * @package midgard.admin.asgard
+ */
 class midgard_admin_asgard_navigation extends midcom_baseclasses_components_purecode
 {
 
@@ -169,37 +181,38 @@ class midgard_admin_asgard_navigation extends midcom_baseclasses_components_pure
                         $selected = true;
                         $css_class .= ' selected';
                     }
-
+                    
                     if ($child->guid == $this->_object->guid)
                     {
                         $css_class .= ' current';
                     }
-
+                    
                     $this->shown_objects[$child->guid] = true;
-
+                    
                     echo "{$prefix}    <li class=\"{$css_class}\">";
 
+                    $label = htmlspecialchars($ref->get_object_label($child));
                     $icon = $ref->get_object_icon($child);
                     if (empty($label))
                     {
                         $label = "#{$child->id}";
                     }
-
+                    
                     echo "<a href=\"{$_MIDGARD['self']}__mfa/asgard/object/view/{$child->guid}/\" title=\"GUID: {$child->guid}, ID: {$child->id}\">{$icon}{$label}</a>\n";
-
+                    
 
                     if ($selected)
                     {
                         $this->_list_child_elements($child, "{$prefix}        ", $level+1);
                     }
-
+                    
                     echo "{$prefix}    </li>\n";
                 }
             }
             echo "{$prefix}</ul>\n";
         }
     }
-
+    
     function _draw_plugins()
     {
         $this->_request_data['chapter_name'] = $_MIDCOM->i18n->get_string('asgard plugins', 'midgard.admin.asgard');
@@ -223,13 +236,20 @@ class midgard_admin_asgard_navigation extends midcom_baseclasses_components_pure
             midcom_show_style('midgard_admin_asgard_navigation_section_footer');
         }
     }
-
+    
     function draw()
     {
-
         $this->_draw_plugins();
         $this->_request_data['chapter_name'] = $_MIDCOM->i18n->get_string('midgard objects', 'midgard.admin.asgard');
         midcom_show_style('midgard_admin_asgard_navigation_chapter');
+        
+        // Use a different method for displaying the navigation
+        if (midgard_admin_asgard_plugin::get_preference('navigation_type') === 'dropdown')
+        {
+            $this->_draw_select_navigation();
+            return;
+        }
+        
         if (!empty($this->_object_path))
         {
             $root_object = $_MIDCOM->dbfactory->get_object_by_guid($this->_object_path[0]);
@@ -317,6 +337,118 @@ class midgard_admin_asgard_navigation extends midcom_baseclasses_components_pure
                     echo "</ul>\n";
                 }
             }
+            midcom_show_style('midgard_admin_asgard_navigation_section_footer');
+        }
+    }
+    
+    function _draw_select_navigation()
+    {
+        if (!empty($this->_object_path))
+        {
+            $root_object = $_MIDCOM->dbfactory->get_object_by_guid($this->_object_path[0]);
+            $this->_request_data['root_object'] =& $root_object;
+            $this->_request_data['navigation_type'] = $root_object->__new_class_name__;
+        }
+        elseif (isset($this->expanded_root_types[0]))
+        {
+            $this->_request_data['navigation_type'] = $this->expanded_root_types[0];
+        }
+        else
+        {
+            $this->_request_data['navigation_type'] = '';
+        }
+        
+        $label_mapping = Array();
+        
+        foreach ($this->root_types as $root_type)
+        {
+            $ref = $this->_get_reflector($root_type);
+            $label_mapping[$root_type] = $ref->get_class_label();
+        }
+        asort($label_mapping);
+        
+        $this->_request_data['label_mapping'] = $label_mapping;
+        $this->_request_data['expanded_root_types'] = $this->expanded_root_types;
+        
+        midcom_show_style('midgard_admin_asgard_navigation_sections');
+        
+        // Stop here if there is no MgdSchema object path to show
+        if (!$this->_request_data['navigation_type'])
+        {
+            return;
+        }
+        
+        if (in_array($this->_request_data['navigation_type'], $this->expanded_root_types))
+        {
+            $this->_request_data['section_url'] = "{$_MIDGARD['self']}__mfa/asgard/{$this->_request_data['navigation_type']}";
+        }
+        else
+        {
+            $this->_request_data['section_url'] = "{$_MIDGARD['self']}__mfa/asgard/{$this->_request_data['navigation_type']}";
+        }
+        
+        $ref = $this->_get_reflector($this->_request_data['navigation_type']);
+        
+        // Show the navigation of the requested object
+        $root_objects = $ref->get_root_objects();
+        
+        $this->_request_data['section_url'] = "{$_MIDGARD['self']}__mfa/asgard/{$this->_request_data['navigation_type']}";
+        $this->_request_data['section_name'] = $ref->get_class_label();
+        
+        if (count($root_objects) > 0)
+        {
+            midcom_show_style('midgard_admin_asgard_navigation_section_header');
+            echo "<ul class=\"midgard_admin_asgard_navigation\">\n";
+     
+            $object_label_mapping = Array();
+
+            $i = 0;
+            foreach ($root_objects as $object)
+            {
+                $object_label_mapping[$i] = $ref->get_object_label($object);
+                $i++;
+            }
+            asort($object_label_mapping);
+
+            foreach ($object_label_mapping as $index => $label)
+            {
+                $object =& $root_objects[$index];
+                $selected = false;
+                $css_class = get_class($object);
+                if (in_array($object->guid, $this->_object_path))
+                {
+                    $selected = true;
+                    $css_class .= ' selected';
+                }
+                
+                if (   is_object($this->_object)
+                    && $object->guid == $this->_object->guid)
+                {
+                    $css_class .= ' current';
+                }
+                $this->shown_objects[$object->guid] = true;
+                
+                echo "    <li class=\"{$css_class}\">";
+
+                $label = htmlspecialchars($label);
+                $icon = $ref->get_object_icon($object);
+                
+                if (empty($label))
+                {
+                    $label = "#oid_{$object->id}";
+                }
+                
+                echo "<a href=\"{$_MIDGARD['self']}__mfa/asgard/object/view/{$object->guid}/\" title=\"GUID: {$object->guid}, ID: {$object->id}\">{$icon}{$label}</a>\n";
+
+                if ($selected)
+                {
+                    $this->_list_child_elements($root_object);
+                }
+                
+                echo "    </li>\n";
+            }
+            
+            echo "</ul>\n";
             midcom_show_style('midgard_admin_asgard_navigation_section_footer');
         }
     }
