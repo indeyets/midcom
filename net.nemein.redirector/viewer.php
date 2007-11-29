4d88bffbb1d9599eb9e6f0ef124254261721df16
Henri Bergius <henri.bergius@iki.fi>
2007-11-29 14:51:45 +0000

    Add new __PREFIX__ replacement rule and clean w_i's [5566]
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13684 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.redirector/viewer.php
+++ net.nemein.redirector/viewer.php
@@ -62,34 +62,44 @@ class net_nemein_redirector_viewer extends midcom_baseclasses_components_request
             case 'url':
                 if ($this->_config->get('redirection_url') != '')
                 {
-					$url = $this->_config->get('redirection_url');
-					
-					// If the replace option is selected
-					if($this->_config->get('replace_lang') == "1")
-					{
-						$content_language = mgd_get_lang();
-						
-						$qb = new midgard_query_builder('midgard_language');
-						$qb->add_constraint('id', '=', $content_language);
-						$lang = $qb->execute();
-						
-						$default_lang_code = "en";
-						$hosts = $_MIDCOM->i18n->get_language_hosts();
-						foreach($hosts as $k => $host) {
-							if($host->prefix != "" && $host->lang == 0) {
-								$default_lang_code = str_replace("/","",$host->prefix);
-							}
-						}
-						$lang_code = $lang[0]->code;
-						if($lang_code == "") {
-							$lang_code = $default_lang_code;
-						}
+                    $url = $this->_config->get('redirection_url');
+                    
+                    // If the replace option is selected
+                    if (strpos($url, '__LANG__') !== false)
+                    {
+                        $content_language = mgd_get_lang();
+                        
+                        $qb = new midgard_query_builder('midgard_language');
+                        $qb->add_constraint('id', '=', $content_language);
+                        $lang = $qb->execute();
+                        
+                        $default_lang_code = 'en';
+                        $hosts = $_MIDCOM->i18n->get_language_hosts();
+                        foreach($hosts as $k => $host) 
+                        {
+                            if (   $host->prefix != '' 
+                                && $host->lang == 0) 
+                            {
+                                $default_lang_code = str_replace('/', '', $host->prefix);
+                            }
+                        }
+                        $lang_code = $lang[0]->code;
+                        if($lang_code == '') 
+                        {
+                            $lang_code = $default_lang_code;
+                        }
 
-						// replace LANG string from redirection_url to current lang name
-						$url = str_replace("LANG",$lang_code,$url);
-					}
-										
-                    $_MIDCOM->relocate($url);
+                        // replace LANG string from redirection_url to current lang name
+                        $url = str_replace('__LANG__', $lang_code, $url);
+                    }
+
+                    // Support varying host prefixes
+                    if (strpos($url, '__PREFIX__') !== false)
+                    {
+                        $url = str_replace('__PREFIX__', $_MIDGARD['self'], $url);
+                    }
+        
+                    $_MIDCOM->relocate($url, $this->_config->get('redirection_code'));
                     // This will exit                    
                 }
                 // Otherwise fall-through to config
