9a551e6c21662635ddc5de6bb880f0f3ff742570
Juhana Hirvilahti <ihirvila@cc.hut.fi>
2007-12-03 08:13:14 +0000

    Removed style/plugins
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13714 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.wizards/style/plugins/koe.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This is the styleelement I use to show the index
- * Use this to get variables etc from the handler:
- */
-//$data =& $_MIDCOM->get_custom_context_data('request_data');
-?>
-<h1>JEE</h1>
-
