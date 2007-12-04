0952af0fd56e51e4215a8b6fa23ec96be0243043
Arttu Manninen <arttu@kaktus.cc>
2007-12-04 14:16:22 +0000

    Added support for showing wikipages in navigation on request
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13763 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.wiki/navigation.php
+++ net.nemein.wiki/navigation.php
@@ -17,14 +17,54 @@ class net_nemein_wiki_navigation  extends midcom_baseclasses_components_navigati
     /**
      * Simple constructor, calls base class.
      */
-	function net_nemein_wiki_navigation() 
+    function net_nemein_wiki_navigation() 
     {
         parent::midcom_baseclasses_components_navigation();
     }
-
+    
+    /**
+     * Get the leaves set to be displayed in navigation
+     * 
+     * @access public
+     * @return array
+     */
     function get_leaves()
     {
+        // Get the required information with midgard_collector
+        $qb = midcom_db_article::new_query_builder();
+        $qb->add_constraint('topic', '=', $this->_topic->id);
+        $qb->add_constraint('view', '=', 1);
+        
+        // Set the order of navigation
+        $qb->add_order('metadata.score', 'DESC');
+        $qb->add_order('title');
+        $qb->add_order('name');
+        
+        // Return an empty result set
+        if ($qb->count() === 0)
+        {
+            return array();
+        }
+        
+        
         $leaves = array();
+        
+        // Get the leaves
+        foreach ($qb->execute() as $article)
+        {
+            $leaves[$article->id] = array
+            (
+                MIDCOM_NAV_URL => $article->name,
+                MIDCOM_NAV_NAME => ($article->title) ? $article->title : $article->name,
+                MIDCOM_NAV_GUID => $article->guid,
+                MIDCOM_META_CREATED => $article->metadata->created,
+                MIDCOM_META_CREATOR => $article->metadata->creator,
+                MIDCOM_META_EDITED => $article->metadata->revised,
+                MIDCOM_META_EDITOR => $article->metadata->revisor,
+            );
+        }
+        
+        // Finally return the leaves
         return $leaves;
     }
 }
