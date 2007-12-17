038d4b850b13e039832d7bc2842879403c0d30e1
Andreas Flack <flack@contentcontrol-berlin.de>
2007-12-17 18:31:33 +0000

    provide better feedback when dir cannot be created and don't try to continue
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13928 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.filesync/midcom/interfaces.php
+++ midcom.helper.filesync/midcom/interfaces.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package midcom.helper.filesync 
+ * @package midcom.helper.filesync
  * @author The Midgard Project, http://www.midgard-project.org
  * @copyright The Midgard Project, http://www.midgard-project.org
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
@@ -8,7 +8,7 @@
 
 /**
  * This is the interface class for midcom.helper.filesync
- * 
+ *
  * @package midcom.helper.filesync
  */
 class midcom_helper_filesync_interface extends midcom_baseclasses_components_interface
@@ -29,7 +29,7 @@ class midcom_helper_filesync_interface extends midcom_baseclasses_components_int
             'exporter.php',
             'importer.php',
         );
-        
+
         // Load all libraries used by component here
         $this->_autoload_libraries = array();
     }
@@ -39,20 +39,28 @@ class midcom_helper_filesync_interface extends midcom_baseclasses_components_int
         $path = $this->_config->get('filesync_path');
         if (!file_exists($path))
         {
-            mkdir($path);
+	        if (! mkdir($path))
+	        {
+	            $_MIDCOM->generate_error(MIDCOM_ERRCRIT, "Failed create directory {$path}. Reason: " . $php_errormsg);
+	            // This will exit.
+	        }
         }
-        
+
         if (substr($path, -1) != '/')
         {
             $path .= '/';
         }
-        
+
         $module_dir = "{$path}{$prefix}";
         if (!file_exists($module_dir))
         {
-            mkdir($module_dir);
+ 	        if (! mkdir($module_dir))
+	        {
+	            $_MIDCOM->generate_error(MIDCOM_ERRCRIT, "Failed create directory {$module_dir}. Reason: " . $php_errormsg);
+	            // This will exit.
+	        }
         }
-        
+
         return "{$module_dir}/";
     }
 }
