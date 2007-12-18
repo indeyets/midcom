72668941262ffe09b3c0879bafb7a2d6e7e0a3b9
Andreas Flack <flack@contentcontrol-berlin.de>
2007-12-18 14:50:47 +0000

    export all writable snippetdirs in the current sitegroup, not just sitegroup-config
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13968 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.filesync/exporter/snippet.php
+++ midcom.helper.filesync/exporter/snippet.php
@@ -15,14 +15,14 @@
 class midcom_helper_filesync_exporter_snippet extends midcom_helper_filesync_exporter
 {
     var $root_dir = '';
-    
+
     function midcom_helper_filesync_exporter_snippet()
     {
         parent::midcom_helper_filesync_exporter();
-        
+
         $this->root_dir = midcom_helper_filesync_interface::prepare_dir('snippets');
     }
-    
+
     function read_snippetdir($snippetdir, $path)
     {
         $snippetdir_path = "{$path}{$snippetdir->name}";
@@ -30,21 +30,21 @@ class midcom_helper_filesync_exporter_snippet extends midcom_helper_filesync_exp
         {
             mkdir($snippetdir_path);
         }
-        
+
         $snippet_qb = midcom_baseclasses_database_snippet::new_query_builder();
         $snippet_qb->add_constraint('up', '=', $snippetdir->id);
         $snippets = $snippet_qb->execute();
         foreach ($snippets as $snippet)
         {
-            // TODO: Multilang support?     
+            // TODO: Multilang support?
             file_put_contents("{$snippetdir_path}/{$snippet->name}.php", $snippet->code);
-            
+
             if (!empty($snippet->doc))
             {
                 file_put_contents("{$snippetdir_path}/{$snippet->name}.txt", $snippet->doc);
             }
         }
-        
+
         $dir_qb = midcom_baseclasses_database_snippetdir::new_query_builder();
         $dir_qb->add_constraint('up', '=', $snippetdir->id);
         $dirs = $dir_qb->execute();
@@ -53,22 +53,21 @@ class midcom_helper_filesync_exporter_snippet extends midcom_helper_filesync_exp
             $this->read_snippetdir($dir, "{$snippetdir_path}/");
         }
     }
-    
-    function read_root($snippetpath)
-    {
-        $snippetdir = new midcom_baseclasses_database_snippetdir();
-        $snippetdir->get_by_path($snippetpath);
-        if (!$snippetdir->guid)
-        {
-            return null;
-        }
-        
-        $this->read_snippetdir($snippetdir, $this->root_dir);
-    }
- 
+
     function export()
     {
-        $this->read_root($GLOBALS['midcom_config']['midcom_sgconfig_basedir']);
+      $qb = midcom_baseclasses_database_snippetdir::new_query_builder();
+      $qb->add_constraint('up', '=', 0);
+      $qb->add_constraint('sitegroup', '=', $_MIDCOM->midgard->sitegroup);
+      $rootdirs = $qb->execute();
+      foreach ($rootdirs as $rootdir)
+      {
+          if ($_MIDCOM->auth->can_do('midgard:update', $rootdir))
+          {
+              $this->read_snippetdir($rootdir, $this->root_dir);
+          }
+      }
     }
+
 }
 ?>
