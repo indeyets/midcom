11a72ca260bb509d8857c657e1e68cce49a5e017
Eero af Heurlin <eero.afheurlin@iki.fi>
2007-12-19 13:37:22 +0000

    saner locale handling with comments explaining various issues
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@13990 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.calendar/functions.php
+++ net.nemein.calendar/functions.php
@@ -16,17 +16,28 @@
  */
 function net_nemein_calendar_functions_daylabel($label='start', $start, $end , $add_time = true, $add_year = false)
 {
+    /** 
+     * If mucking about with locales at least have the courtesy to return
+     * the value to what it used to be, here we read the current value,
+     * see the end of function for restoring it
+     */
+    $current_time_locale = setlocale(LC_TIME, '0');
+
+    /**
+     * Make double sure LC_TIME is set according to current language
+     *
+     * This should not be neccessary, also there might be a case where someone
+     * wants to set LC_TIME but not set actual language, this will negate that attempt
+     */
     $language = $_MIDCOM->i18n->get_current_language();
     $language_db = $_MIDCOM->i18n->get_language_db();
-    // fix that at least works very well on debian. 
-    if ( $language_db[$language]['encoding']  =='UTF-8' )
-    {
-        setlocale(LC_TIME, $language_db[$language]['locale']. ".UTF-8");
-    } 
-    else
-    {
-        setlocale(LC_TIME, $language_db[$language]['locale']);
-    }
+    /**
+     * NOTE: setlocale can take an array of locales as value, it will use
+     * the first name valid for the system, thus if some charset or such
+     * variant is not present, add it to the language db, do not make up weird
+     * variable rewriting rules here
+     */
+    setlocale(LC_TIME, $language_db[$language]['locale']);
 
     $daylabel = '';
 
@@ -66,6 +77,10 @@ function net_nemein_calendar_functions_daylabel($label='start', $start, $end , $
             $daylabel .= date('H:i', $end);        
         }
     }
+    /**
+     * Restore original LC_TIME value, see beginning of function
+     */
+    setlocale(LC_TIME, $current_time_locale);
     return $daylabel;
 }
 
