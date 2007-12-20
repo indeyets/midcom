6a5e016dc93b0d095b3f5b4ec335e5a7c7c86336
Arttu Manninen <arttu@kaktus.cc>
2007-12-20 09:22:19 +0000

    New features and fieldset grouping for preferences + new locales.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14014 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/style/midgard_admin_asgard_middle.php
+++ midgard.admin.asgard/style/midgard_admin_asgard_middle.php
@@ -26,16 +26,23 @@
                     ?>
                 </div>
                 <div id="content"&(content_offset:h);>
+<?php
+if (midgard_admin_asgard_plugin::get_preference('enable_quicklinks'))
+{
+?>
                     <div class="quicklinks">
                         <ul>
                             <li>
-                                <a href="&(prefix);" title="<?php echo $_MIDCOM->i18n->get_string('user preferences', 'midgard.admin.asgard'); ?>"><img src="<?php echo MIDCOM_STATIC_URL; ?>/stock-icons/16x16/configuration.png" alt="<?php echo $_MIDCOM->i18n->get_string('user preferences', 'midgard.admin.asgard'); ?>" /></a>
+                                <a href="&(prefix);__mfa/asgard/preferences/" title="<?php echo $_MIDCOM->i18n->get_string('user preferences', 'midgard.admin.asgard'); ?>"><img src="<?php echo MIDCOM_STATIC_URL; ?>/stock-icons/16x16/configuration.png" alt="<?php echo $_MIDCOM->i18n->get_string('user preferences', 'midgard.admin.asgard'); ?>" /></a>
                             </li>
                             <li>
                                 <a href="&(prefix);" title="<?php echo $_MIDCOM->i18n->get_string('back to site', 'midgard.admin.asgard'); ?>"><img src="<?php echo MIDCOM_STATIC_URL; ?>/stock-icons/16x16/gohome.png" alt="<?php echo $_MIDCOM->i18n->get_string('back to site', 'midgard.admin.asgard'); ?>" /></a>
                             </li>
                         </ul>
                     </div>
+<?php
+}
+?>
                     <div class="page-title">
                         <?php
                         echo "<h1>";
