dc941ada4b4dfcd4a7dd2438f557020eb9341b3f
Henri Bergius <henri.bergius@iki.fi>
2008-01-03 09:59:40 +0000

    Add "first post" to old threads
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14188 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.discussion/exec/update-caches.php
+++ net.nemein.discussion/exec/update-caches.php
@@ -4,7 +4,7 @@ $debug = false;
 $_MIDCOM->auth->require_admin_user();
 
 // Set all posts to "NEW" status if they don't have a status
-$post_qb = new MidgardQueryBuilder('net_nemein_discussion_post');
+$post_qb = new midgard_query_builder('net_nemein_discussion_post');
 $post_qb->add_constraint('status', '=', 0);
 //$post_qb->set_limit(400);
 $posts = @$post_qb->execute();
@@ -20,21 +20,28 @@ foreach ($posts as $post)
 }
 
 // Update thread caches
-$qb = new MidgardQueryBuilder('net_nemein_discussion_thread');
+$qb = new midgard_query_builder('net_nemein_discussion_thread');
 $threads = $qb->execute();
 foreach ($threads as $thread)
 {   
     $posts = null;
-    $post_qb = new MidgardQueryBuilder('net_nemein_discussion_post');
+    $post_qb = new midgard_query_builder('net_nemein_discussion_post');
     $post_qb->add_constraint('thread', '=', $thread->id);
     $post_qb->add_constraint('status', '>=', 3);
+    $post_qb->add_order('metadata.published', 'ASC');
     // TODO: Add moderation checks
     $posts = $post_qb->execute();
     $latest_post = null;
+    $first_post = null;
     if ($thread->latestpost)
     {
         $latest_post = new net_nemein_discussion_thread();
         $latest_post->get_by_id($thread->latestpost);
+    }
+    if ($thread->firstpost)
+    {
+        $first_post = new net_nemein_discussion_thread();
+        $first_post->get_by_id($thread->firstpost);
     }
     foreach ($posts as $post)
     {
@@ -43,18 +50,33 @@ foreach ($threads as $thread)
         {
             $latest_post = $post;
         }
+        
+        if (   is_null($first_post)
+            || $post->metadata->published < $first_post->metadata->published)
+        {
+            $first_post = $post;
+        }
     }
     
     if (   count($posts) != $thread->posts
         || (   is_object($latest_post)
             && $latest_post->id != $thread->latestpost)
-        || $thread->latestposttime != strtotime($latest_post->metadata->published))
+        || (   is_object($latest_post)
+            && $thread->latestposttime != strtotime($latest_post->metadata->published))
+        || (   is_object($first_post)
+            && $first_post->id != $thread->firstpost))
     {
         $thread->posts = count($posts);
         $thread->latestpost = $latest_post->id;
+        
+        if ($first_post)
+        {
+            $thread->firstpost = $first_post->id;
+        }
+        
         $thread->latestposttime = strtotime($latest_post->metadata->published);
         
-        echo "Setting post count for thread \"{$thread->title}\" to {$thread->posts} and latest post to #{$thread->latestpost}<br />\n";
+        echo "Setting post count for thread \"{$thread->title}\" to {$thread->posts}, latest post to #{$thread->latestpost} and first post to #{$thread->firstpost}<br />\n";
         flush();
         
         if (!$debug);
