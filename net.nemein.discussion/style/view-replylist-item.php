16b7bb0856768ec99ce2114f02c23617a0d78788
Henri Bergius <henri.bergius@iki.fi>
2008-01-03 09:24:32 +0000

    Use correct metadata property
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14183 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.discussion/style/view-replylist-item.php
+++ net.nemein.discussion/style/view-replylist-item.php
@@ -1,5 +1,4 @@
 <?php
-//$data =& $_MIDCOM->get_custom_context_data('request_data');
 $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
 $post =& $data['post'];
 $thread = new net_nemein_discussion_thread_dba($post->thread);
@@ -11,6 +10,6 @@ $forum = $nav->get_node($thread->node);
     <a class="subject url" href="&(forum[MIDCOM_NAV_FULLURL]);read/&(post.guid);.html">&(post.subject);</a>
     <span class="commenter">&(post.sendername);</span>
     <?php
-    echo "<abbr class=\"dtcommented\" title=\"".gmdate('Y-m-d\TH:i:s\Z', $post->created). "\">".strftime('%x %X', $post->created)."</abbr>\n";
+    echo "<abbr class=\"dtcommented\" title=\"".gmdate('Y-m-d\TH:i:s\Z', $post->metadata->published). "\">".strftime('%x %X', $post->metadata->published)."</abbr>\n";
     ?>
 </li>
