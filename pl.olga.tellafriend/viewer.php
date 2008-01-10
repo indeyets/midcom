035263f8c3bd738c3c57105ffc071da6fa0498ea
Andreas Flack <flack@contentcontrol-berlin.de>
2008-01-10 19:25:35 +0000

    PHPdoc fixes again
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14365 62f49d1c-1c16-0410-9156-d311ce72abac

--- pl.olga.tellafriend/viewer.php
+++ pl.olga.tellafriend/viewer.php
@@ -12,39 +12,36 @@
  * Sample usage. You should use it from within the component style like
  *  "show-article". First, load and instantiate the library:
  *
- * <pre>
- * <?php
+ * <code>
  * $_MIDCOM->load_library("pl.olga.tellafriend");
  * $taf = new pl_olga_tellafriend();
- * ?>
- * </pre>
+ * </code>
  *
  * Then prepare a form. You should embed the form into a page you want to
  * notify about. I use layers:
  *
- * <pre>
- * &lt;div style="visibility:hidden" id="taf"&gt;
+ * <code>
+ * <div style="visibility:hidden" id="taf">
  * <?php echo $taf->controller->display_form(); ?>
- * &lt;/div&gt;
- * &lt;a href="javascript:;" onclick="your_favourite_show_hide_layer_js_function()&gt;Notify a friend&lt;/a&gt;
- * </pre>
+ * </div>
+ * <a href="javascript:;" onclick="your_favourite_show_hide_layer_js_function()">Notify a friend</a>;
+ * </code>
  *
  * In case you would like to have the form shown when submitted form was
  *  incomplete, you can detect the state and set visibility of a layer:
  *
- * <pre>
+ * <code>
  * <?php
- * $taf_visible = ($taf->result=='edit' && $GLOBALS["_POST"]["_qf__pl_olga_tellafriend"])?"visible":"hidden";
+ * $taf_visible = ($taf->result=='edit' && $GLOBALS["_POST"]["_qf__pl_olga_tellafriend"]) ? "visible" : "hidden";
  * ?>
- * &lt;div style="visibility:&(taf_visible);" id="taf"&gt;
+ * <div style="visibility:&(taf_visible);" id="taf">
  * <?php echo $taf->controller->display_form(); ?>
- * &lt;/div&gt;
- * &lt;a href="javascript:;" onclick="your_favourite_show_hide_layer_js_function()&gt;Notify a friend&lt;/a&gt;
- * </pre>
+ * </div>
+ * <a href="javascript:;" onclick="your_favourite_show_hide_layer_js_function()">Notify a friend>/a>
+ * </code>
  *
  * @package pl.olga.tellafriend
  */
-
 class pl_olga_tellafriend  extends midcom_baseclasses_components_purecode
 {
 
@@ -67,14 +64,14 @@ class pl_olga_tellafriend  extends midcom_baseclasses_components_purecode
         $this->result = $this->controller->process_form();
 
         if ($this->result == 'save')
-        {      
+        {
             // Load form values. Hmm.. Shall I call DM or FM?
             $dm = $this->controller->datamanager->types;
-            
+
             // Most dirty hack ever. If we got 'save' we got form POST'ed. So the referer is
             // the page (URL) we want to notify about :)
             $link_url = $_SERVER['HTTP_REFERER'];
-            
+
             $conf_url = $this->_config->get('link_url');
             if ($conf_url)
             {
@@ -84,24 +81,24 @@ class pl_olga_tellafriend  extends midcom_baseclasses_components_purecode
             {
                 $link_url = $_MIDCOM->get_host_prefix() . $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
             }
-            
+
             $from = "\"{$dm['sender_name']->value}\" <{$dm['sender']->value}>";
             if (empty($from))
             {
                 $from = $this->_config->get('mail_sender_email');
             }
-            
+
             $subject = str_replace("__SENDER_NAME__", $dm['sender']->value, $this->_config->get('mail_subject'));
             $subject = str_replace("__LINK_URL__", $link_url, $subject);
-            
+
             $content = str_replace("__SENDER_NAME__", $dm['sender_name']->value, $this->_config->get('sysmsg'));
             $content = str_replace("__SENDER_EMAIL__", $dm['sender']->value, $content);
             $content = str_replace("__SUBJECT__", $subject, $content);
             $content = str_replace("__LINK_URL__", $link_url, $content);
             $content = str_replace("__DATETIME__", strftime("%x %X", time()), $content);
             $content = str_replace("__MESSAGE__", $dm['comment']->value, $content);
-            
-            $mail = new org_openpsa_mail();            
+
+            $mail = new org_openpsa_mail();
             $mail->to = $dm['recipient']->value;
             $mail->from = $from;
             $mail->subject = $subject;
