46a660593d2901cb88b88ec3ac062b14f58df453
Eero af Heurlin <eero.afheurlin@iki.fi>
2008-01-11 13:40:16 +0000

    forward port r14375
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14376 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.core/support/midcom_services_cron.sh
+++ midcom.core/support/midcom_services_cron.sh
@@ -16,8 +16,12 @@ SITES="example.com/test https://secure.example.com"
 # alternatively use elinks, you also specify other options here
 # like "lynx -auth=username:password" for HTTP basic-auth
 LYNX="lynx" 
+STAT="stat -c %Z" # how to get file timestamp
+DATE="date +%s" # how to get system timestamp
+LIFETIME=3600 # Max pidfile age in seconds before it's considered stale
 
-if [ "$1" == "" ]; then
+if [ "$1" == "" ]
+then
     URL_SUFFIX=""
     PIDFILE="/var/run/midcom_services_cron_everymin.pid"
     PIDFILE2=""
@@ -27,23 +31,52 @@ else
     PIDFILE2="/var/run/midcom_services_cron_everymin.pid"
 fi  
 
-# TODO: figure out a locking mechanism that ensures only one instance
-# is running at a time, without missing any valid runs
+function check_pidfile
+{
+    if [ -f $1 ]
+    then
+        MODIFIED=`$STAT $1`
+        NOW=`$DATE`
+        AGE=`expr $NOW - $MODIFIED`
+        #echo "MODIFIED=$MODIFIED NOW=$NOW AGE=$AGE LIFETIME=$LIFETIME"
+        if [ "$AGE" -lt "$LIFETIME" ]
+        then
+            return 1
+        fi
+        echo "Lock $1 is stale, removing"
+        # PONDER: Should we kill the process ??, the line below should do the trick
+        # kill -9 `cat $1`
+        rm -f $1
+        return 0
+    fi
+    return 0
+}
 
 # Make sure we only have one copy of the type running at a given time
-test -f $PIDFILE && exit 1
+check_pidfile $PIDFILE
+PIDSTAT=$?
+if [  "$PIDSTAT" != "0" ]
+then
+    exit 1
+fi
 echo $$ > $PIDFILE
-# Wait until everymin script has completed if it's running
-if [ "$PIDFILE2" != "" ]; then
-    if [ -f $PIDFILE2 ]; then
-        while [ -f $PIDFILE2 ]; do
-            sleep 10
-        done
-    fi
+# Wait untill everymin script has completed if it's running
+if [ "$PIDFILE2" != "" ]
+then
+    check_pidfile $PIDFILE2
+    PIDSTAT2=$?
+    while [  "$PIDSTAT2" != "0" ]
+    do
+        sleep 10
+        check_pidfile $PIDFILE2
+        PIDSTAT2=$?
+    done
 fi
 
-for SITE in $SITES; do
-    if [ `expr match "$SITE" 'https*'` -eq 0 ]; then
+for SITE in $SITES
+do
+    if [ `expr match "$SITE" 'https*'` -eq 0 ]
+    then
         URL="http://$SITE/midcom-exec-midcom/cron.php$URL_SUFFIX"
     else
         URL="$SITE/midcom-exec-midcom/cron.php$URL_SUFFIX"
