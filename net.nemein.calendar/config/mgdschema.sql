941d9a0fdc3a3798751b712c4ab13b4aab659882
Eero af Heurlin <eero.afheurlin@iki.fi>
2008-01-21 14:10:31 +0000

    forward port r14524, r14527 & r14528
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14533 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.calendar/config/mgdschema.sql
+++ net.nemein.calendar/config/mgdschema.sql
@@ -1,5 +1,30 @@
 #Creating columns created for net.nemein.calendar
 ## net_nemein_calendar_event
+CREATE TABLE net_nemein_calendar_event (
+  `id` int(11) NOT NULL auto_increment,
+  `node` int(11) NOT NULL default '0',
+  `up` int(11) NOT NULL default '0',
+  `type` int(11) NOT NULL default '0',
+  `sitegroup` int(11) NOT NULL,
+  `guid` varchar(80) NOT NULL,
+  `name` varchar(255) NOT NULL default '',
+  `start` datetime NOT NULL default '0000-00-00 00:00:00',
+  `end` datetime NOT NULL default '0000-00-00 00:00:00',
+  `start` datetime NOT NULL default '0000-00-00 00:00:00',
+  `openregistration` datetime NOT NULL default '0000-00-00 00:00:00',
+  `closeregistration` datetime NOT NULL default '0000-00-00 00:00:00',
+  `location` varchar(255) NOT NULL default '',
+  `extra` longtext NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `net_nemein_calendar_event_guid_idx` (`guid`),
+  KEY `net_nemein_calendar_event_sitegroup_idx` (`sitegroup`)
+);
+CREATE INDEX `net_nemein_calendar_event_node_idx` ON net_nemein_calendar_event (`node`);
+CREATE INDEX `net_nemein_calendar_event_up_idx` ON net_nemein_calendar_event (`up`);
+CREATE INDEX `net_nemein_calendar_event_start_idx` ON net_nemein_calendar_event (`start`);
+CREATE INDEX `net_nemein_calendar_event_end_idx` ON net_nemein_calendar_event (`end`);
+CREATE INDEX `net_nemein_calendar_event_openregistration_idx` ON net_nemein_calendar_event (`openregistration`);
+CREATE INDEX `net_nemein_calendar_event_closeregistration_idx` ON net_nemein_calendar_event (`closeregistration`);
 
 CREATE TABLE net_nemein_calendar_event_i (
   id int(11) NOT NULL auto_increment,
--- net.nemein.calendar/event.php
+++ net.nemein.calendar/event.php
@@ -14,21 +14,43 @@
  */
 class net_nemein_calendar_event_dba extends __net_nemein_calendar_event_dba
 {
-    function net_nemein_calendar_event($guid = null) 
+    function net_nemein_calendar_event_dba($guid = null) 
     {
-        return parent::__net_nemein_calendar_event($guid);
+        return parent::__net_nemein_calendar_event_dba($guid);
     }
 
     function _on_created()
-    {    
-        midcom_baseclasses_core_dbobject::generate_urlname($this);
-        return parent::_on_created();
+    {
+        if (isset($GLOBALS['net_nemein_calendar_event_dba__on_created_loop_{$this->guid}']))
+        {
+            debug_push_class(__CLASS__, __FUNCTION__);
+            debug_add("Detected _on_updated loop on #{$this->id}", MIDCOM_LOG_ERROR);
+            debug_pop();
+        }
+        else
+        {
+            $GLOBALS['net_nemein_calendar_event_dba__on_created_loop_{$this->guid}'] = true;
+            midcom_baseclasses_core_dbobject::generate_urlname($this);
+            unset($GLOBALS['net_nemein_calendar_event_dba__on_updated_loop_{$this->guid}']);
+        }
+        return true;
     }
-    
+
     function _on_updated()
-    {    
-        midcom_baseclasses_core_dbobject::generate_urlname($this);
-        return parent::_on_updated();
+    {
+        if (isset($GLOBALS['net_nemein_calendar_event_dba__on_updated_loop_{$this->guid}']))
+        {
+            debug_push_class(__CLASS__, __FUNCTION__);
+            debug_add("Detected _on_updated loop on #{$this->id}", MIDCOM_LOG_ERROR);
+            debug_pop();
+        }
+        else
+        {
+            $GLOBALS['net_nemein_calendar_event_dba__on_updated_loop_{$this->guid}'] = true;
+            midcom_baseclasses_core_dbobject::generate_urlname($this);
+            unset($GLOBALS['net_nemein_calendar_event_dba__on_updated_loop_{$this->guid}']);
+        }
+        return true;
     }
 }
 ?>
