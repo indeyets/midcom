539a199c4d2283d18ccf9a9c667c819f278a70bb
Eero af Heurlin <eero.afheurlin@iki.fi>
2008-02-06 11:29:37 +0000

    forward port r14752
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14754 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom.helper.replicator/config/mgdschema.sql
@@ -0,0 +1,15 @@
+#replicator_subscription
+CREATE TABLE midcom_helper_replicator_subscription (
+  id int(11) NOT NULL auto_increment,
+  sitegroup int(11) NOT NULL default 0,
+  status int(11) NOT NULL default 0,
+  title varchar(255) NOT NULL default '',
+  description longtext NOT NULL default '',
+  exporter varchar(255) NOT NULL default '',
+  transporter varchar(255) NOT NULL default '',
+# other fields/indexes as ALTER TABLE statements
+  PRIMARY KEY  (id),
+  KEY midcom_helper_replicator_subscription_sitegroup_idx (sitegroup),
+  KEY midcom_helper_replicator_subscription_status_idx (status),
+);
+ALTER TABLE midcom_helper_replicator_subscription ADD COLUMN filtersSerialized longtext NOT NULL default '';
