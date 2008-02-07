18ad2c45837230606099cfa7a890e8ffa88c1920
Eero af Heurlin <eero.afheurlin@iki.fi>
2008-02-07 19:36:26 +0000

    script to normalize white space in midgard pages, page-elements and style-elements in given sitegroup
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@14798 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.core/support/normalize_midgard_whitespace.php
+++ midcom.core/support/normalize_midgard_whitespace.php
@@ -2,43 +2,115 @@
 <?php
 error_reporting(E_ALL);
 require_once('normalize_whitespace_normalizer.php');
-if ($argc < 2)
+if ($argc < 4)
 {
     $name = basename($argv[0]);
-    echo "\nUsage: {$name} <files_list>\n";
+    echo "\nUsage: {$name} <configfile> <username> <password>\n";
     echo "  For example:\n";
-    echo "  {$name} `find ~/svn/midcom/ -name '*.php'` \n\n";
+    echo "  {$name} midgard_sgx 'sgadmin+sgname' 'adminpasswd' \n\n";
+    echo "(you need to specify the MidgardUser and MidgardPassword\n in the midgard_sgx config file)\n\n";
     exit(1);
 }
-$files = array_slice($argv, 1);
-
-if (!function_exists('file_put_contents'))
+$conffile =& $argv[1];
+$username =& $argv[2];
+$password =& $argv[3];
+if (!mgd_config_init($conffile))
 {
-    function file_put_contents($file, &$data)
-    {
-        $fp = fopen($file, 'w');
-        if (!$fp)
-        {
-            return false;
-        }
-        $ret = fwrite($fp, $data);
-        fclose($fp);
-        return $ret;
-    }
+    echo "\nInitialization failed\n\n";
+    exit(1);
+}
+mgd_auth_midgard($username, $password);
+if (!$_MIDGARD['user'])
+{
+    echo "\nAuthentication failed\n\n";
+    exit(1);
+}
+if ($_MIDGARD['sitegroup'] === 0)
+{
+    echo "\nSG0 usage not supported\n\n";
+    exit(1);
 }
 
 $normalizer = new midcom_support_wsnormalizer();
-foreach ($files as $file)
+
+// Pages (PONDER: handle Multilang ??)
+$qb = new midgard_query_builder('midgard_page');
+$qb->add_constraint('sitegroup', '=', $_MIDGARD['sitegroup']);
+$pages = $qb->execute();
+unset($qb);
+foreach ($pages as $page)
 {
-    $data = file_get_contents($file);
-    $normalized = $normalizer->normalize($data);
-    if ($data === $normalized)
+    $normalized = $normalizer->normalize($page->content);
+    if ($normalized === $page->content)
     {
-        unset($data, $normalized);
+        unset($normalized, $page);
         continue;
     }
-    file_put_contents($file, $normalized);
-    unset($data, $normalized);
+    $page->content = $normalized;
+    unset($normalized);
+    if (!$page->update())
+    {
+        echo " Failed to update page #{$page->id} ({$page->name}), " . mgd_errstr() . "\n";
+        unset($page);
+        continue;
+    }
+    //echo "DEBUG: updated page #{$page->id} ({$page->name}), " . mgd_errstr() . "\n";
+    unset($page);
 }
+unset($pages);
+
+// Page-elements (PONDER: handle Multilang ??)
+$qb = new midgard_query_builder('midgard_pageelement');
+$qb->add_constraint('sitegroup', '=', $_MIDGARD['sitegroup']);
+$pageelements = $qb->execute();
+unset($qb);
+foreach ($pageelements as $pageelement)
+{
+    $normalized = $normalizer->normalize($pageelement->value);
+    if ($normalized === $pageelement->value)
+    {
+        unset($normalized, $pageelement);
+        continue;
+    }
+    $pageelement->value = $normalized;
+    unset($normalized);
+    if (!$pageelement->update())
+    {
+        echo " Failed to update page-element #{$pageelement->id} ({$pageelement->name}), " . mgd_errstr() . "\n";
+        unset($pageelement);
+        continue;
+    }
+    //echo "DEBUG: updated page-element #{$pageelement->id} ({$pageelement->name}), " . mgd_errstr() . "\n";
+    unset($pageelement);
+}
+unset($pageelements);
+
+// (Style) elements (PONDER: handle Multilang ??)
+$qb = new midgard_query_builder('midgard_element');
+$qb->add_constraint('sitegroup', '=', $_MIDGARD['sitegroup']);
+$elements = $qb->execute();
+unset($qb);
+foreach ($elements as $element)
+{
+    $normalized = $normalizer->normalize($element->value);
+    if ($normalized === $element->value)
+    {
+        unset($normalized, $element);
+        continue;
+    }
+    $element->value = $normalized;
+    unset($normalized);
+    if (!$element->update())
+    {
+        echo " Failed to update style-element #{$element->id} ({$element->name}), " . mgd_errstr() . "\n";
+        unset($element);
+        continue;
+    }
+    //echo "DEBUG: updated style-element #{$element->id} ({$element->name}), " . mgd_errstr() . "\n";
+    unset($element);
+}
+unset($elements);
+
+
 
 ?>
