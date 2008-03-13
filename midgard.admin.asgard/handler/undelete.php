54db9e34825072c185f8a4c123c375428d72a2b4
Arttu Manninen <arttu@kaktus.cc>
2008-03-13 16:27:37 +0000

    Moved undelete to MidCOM core dbobject.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@15574 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/handler/undelete.php
+++ midgard.admin.asgard/handler/undelete.php
@@ -49,294 +49,6 @@ class midgard_admin_asgard_handler_undelete extends midcom_baseclasses_component
     }
 
     /**
-     * Get an object, deleted or not
-     */
-    function _get_object($guid, $type)
-    {
-        static $objects = array();
-
-        if (!isset($objects[$guid]))
-        {
-            $qb = new midgard_query_builder($type);
-            $qb->add_constraint('guid', '=', $guid);
-            $qb->include_deleted();
-            $results = $qb->execute();
-            if (count($results) == 0)
-            {
-                $objects[$guid] = null;
-            }
-            else
-            {
-                $objects[$guid] = $results[0];
-            }
-        }
-
-        return $objects[$guid];
-    }
-
-    /**
-     * Recover the parameters related to a deleted object
-     *
-     * @access private
-     */
-    function _undelete_parameters($guid)
-    {
-        $qb = new midgard_query_builder('midgard_parameter');
-        $qb->include_deleted();
-        $qb->add_constraint('parentguid', '=', $guid);
-        $qb->add_constraint('metadata.deleted', '=', true);
-        $params = $qb->execute();
-        foreach ($params as $param)
-        {
-            $undeleted = false;
-            if (version_compare(phpversion(), '5.0.0', '<'))
-            {
-                if (call_user_func(array('midgard_parameter', 'undelete'),$param->guid))
-                {
-                    $undeleted = true;
-                }
-            }
-            else
-            {
-                if ($param->undelete())
-                {
-                    $undeleted = true;
-                }
-            }
-            if ($undeleted)
-            {
-                $this->_undeleted_size += $param->metadata->size;
-            }
-        }
-    }
-
-    /**
-     * Recover the attachments related to a deleted object
-     *
-     * @access private
-     */
-    function _undelete_attachments($guid)
-    {
-        $qb = new midgard_query_builder('midgard_attachment');
-        $qb->include_deleted();
-        $qb->add_constraint('parentguid', '=', $guid);
-        $qb->add_constraint('metadata.deleted', '=', true);
-        $atts = $qb->execute();
-        foreach ($atts as $att)
-        {
-            $undeleted = false;
-            if (version_compare(phpversion(), '5.0.0', '<'))
-            {
-                if (call_user_func(array('midgard_attachment', 'undelete'),$att->guid))
-                {
-                    $undeleted = true;
-                }
-            }
-            else
-            {
-                if ($att->undelete())
-                {
-                    $undeleted = true;
-                }
-            }
-            if (!$undeleted)
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('failed undeleting attachment %s, reason %s'), $att->name, mgd_errstr()), 'error');
-            }
-            else
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('attachment %s undeleted'), $att->name, mgd_errstr()), 'ok');
-                $this->_undeleted_size += $att->metadata->size;
-                $this->_undelete_parameters($att->guid);
-            }
-        }
-    }
-
-    /**
-     * Purge the parameters related to a deleted object
-     *
-     * @access private
-     */
-    function _purge_parameters($guid)
-    {
-        $qb = new midgard_query_builder('midgard_parameter');
-        $qb->include_deleted();
-        $qb->add_constraint('parentguid', '=', $guid);
-        $params = $qb->execute();
-        foreach ($params as $param)
-        {
-            if (!$param->metadata->deleted)
-            {
-                $param->delete();
-            }
-
-            if ($param->purge())
-            {
-                $this->_purged_size += $param->metadata->size;
-            }
-            else
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('failed purging attachment %s => %s, reason %s'), $param->domain,$param->name, mgd_errstr()), 'error');
-            }
-        }
-    }
-
-    /**
-     * Purge the attachments related to a deleted object
-     *
-     * @access private
-     */
-    function _purge_attachments($guid)
-    {
-        $qb = new midgard_query_builder('midgard_attachment');
-        $qb->include_deleted();
-        $qb->add_constraint('parentguid', '=', $guid);
-        $atts = $qb->execute();
-        foreach ($atts as $att)
-        {
-            if (!$att->metadata->deleted)
-            {
-                $att->delete();
-            }
-
-            if ($att->purge())
-            {
-                $this->_purged_size += $att->metadata->size;
-                $this->_purge_parameters($att->guid);
-            }
-            else
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('failed purging attachment %s, reason %s'), $att->name, mgd_errstr()), 'error');
-            }
-        }
-    }
-
-    /**
-     * Helper method for undeleting objects
-     *
-     * @access private
-     * @param Array $guids
-     * @param string $type
-     */
-    function _undelete($guids, $type)
-    {
-        $ref = midcom_helper_reflector_tree::get($type);
-        foreach ($guids as $guid)
-        {
-            $object = $this->_get_object($guid, $type);
-            if (is_null($object))
-            {
-                // Purged, skip
-                continue;
-            }
-            $label = $ref->get_label_property();
-
-            $undeleted = false;
-            if (version_compare(phpversion(), '5.0.0', '<'))
-            {
-                if (call_user_func(array($type, 'undelete'), $guid))
-                {
-                    $undeleted = true;
-                }
-            }
-            else
-            {
-                if ($object->undelete($guid))
-                {
-                    $undeleted = true;
-                }
-            }
-
-            if (!$undeleted)
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('failed undeleting %s, reason %s'), "{$type} {$object->$label}", mgd_errstr()), 'error');
-            }
-            else
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('%s undeleted'), "{$type} {$object->$label}", mgd_errstr()), 'ok');
-                $this->_undeleted_size += $object->metadata->size;
-            }
-
-            $this->_undelete_parameters($guid);
-            $this->_undelete_attachments($guid);
-
-            // List all deleted children
-            $children_types = $ref->get_child_objects($object, true);
-
-            foreach ($children_types as $type => $children)
-            {
-                $child_guids = array();
-                foreach ($children as $child)
-                {
-                    if ($child->metadata->deleted)
-                    {
-                        $child_guids[] = $child->guid;
-                    }
-                }
-                $this->_undelete($child_guids, $type);
-            }
-        }
-    }
-
-    /**
-     * Helper method for purging objects
-     *
-     * @access private
-     * @param Array $guids
-     * @param string $type
-     */
-    function _purge($guids, $type)
-    {
-        $ref = midcom_helper_reflector_tree::get($type);
-        foreach ($guids as $guid)
-        {
-            $object = $this->_get_object($guid, $type);
-            if (is_null($object))
-            {
-                // Something wrong
-                continue;
-            }
-
-            // first kill your children
-            $children_types = $ref->get_child_objects($object, true);
-
-            foreach ($children_types as $type => $children)
-            {
-                $child_guids = array();
-                foreach ($children as $child)
-                {
-                    if (!$child->metadata->deleted)
-                    {
-                        $child->delete();
-                    }
-                    $child_guids[] = $child->guid;
-                }
-                $this->_purge($child_guids, $type);
-            }
-
-            // then shoot your dogs
-
-            $this->_purge_parameters($guid);
-            $this->_purge_attachments($guid);
-
-            $label = $ref->get_label_property();
-
-            // now shoot yourself
-
-            if (!$object->purge())
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('failed purging %s, reason %s'), "{$type} {$object->$label}", mgd_errstr()), 'error');
-            }
-            else
-            {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('%s purged'), "{$type} {$object->$label}", mgd_errstr()), 'ok');
-                $this->_purged_size += $object->metadata->size;
-            }
-
-        }
-    }
-
-    /**
      * Trash view
      *
      * @param mixed $handler_id The ID of the handler.
@@ -431,12 +143,12 @@ class midgard_admin_asgard_handler_undelete extends midcom_baseclasses_component
         if (   isset($_POST['undelete']) && !isset($_POST['purge'])
             && is_array($_POST['undelete']))
         {
+            static $undeleted_size = 0;
+            midcom_baseclasses_core_dbobject::undelete($_POST['undelete'], $this->type);
 
-            $this->_undelete($_POST['undelete'], $this->type);
-
-            if ($this->_undeleted_size > 0)
+            if ($undeleted_size > 0)
             {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('in total %s undeleted'), midcom_helper_filesize_to_string($this->_undeleted_size)), 'info');
+                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('in total %s undeleted'), midcom_helper_filesize_to_string($undeleted_size)), 'info');
             }
             $_MIDCOM->relocate("__mfa/asgard/trash/{$this->type}/");
         }
@@ -444,11 +156,12 @@ class midgard_admin_asgard_handler_undelete extends midcom_baseclasses_component
         if (   isset($_POST['purge'])
             && is_array($_POST['undelete']))
         {
-            $this->_purge($_POST['undelete'], $this->type);
+            static $purged_size = 0;
+            midcom_baseclasses_core_dbobject::purge($_POST['undelete'], $this->type);
 
-            if ($this->_purged_size > 0)
+            if ($purged_size)
             {
-                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('in total %s purged'), midcom_helper_filesize_to_string($this->_purged_size)), 'info');
+                $_MIDCOM->uimessages->add($this->_l10n->get('midgard.admin.asgard'), sprintf($this->_l10n->get('in total %s purged'), midcom_helper_filesize_to_string($purged_size)), 'info');
             }
             $_MIDCOM->relocate("__mfa/asgard/trash/{$this->type}/");
         }
