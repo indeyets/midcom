456ae3650d8c01dd9ba1548954ed4478b539d0ec
Jerry Jalava <jerry.jalava@gmail.com>
2008-03-13 14:54:44 +0000

    New method to render ajax add links
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@15566 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.favourites/admin.php
+++ net.nemein.favourites/admin.php
@@ -206,6 +206,101 @@ class net_nemein_favourites_admin
         echo net_nemein_favourites_admin::get_add_link($objectType, $guid, $url, $link_for_anonymous);
     }
     
+    function render_ajax_add_link($objectType, $guid, $url = '', $link_for_anonymous = true, $force_url_prefix = null)
+    {
+        if (   empty($objectType)
+            || empty($guid))
+        {
+            return false;
+        }
+        
+        $url_prefix = '';
+        if (! is_null($force_url_prefix))
+        {
+            $url_prefix = $force_url_prefix;
+        }
+
+        if (empty($url))
+        {
+            $node = midcom_helper_find_node_by_component('net.nemein.favourites');
+            if (!empty($node))
+            {
+                $url = $node[MIDCOM_NAV_FULLURL];
+            }
+        }
+
+        if (empty($url))
+        {
+            return false;
+        }
+
+        $midcom_i18n =& $_MIDCOM->get_service('i18n');
+        $l10n =& $midcom_i18n->get_l10n('net.nemein.favourites');
+
+        $data = net_nemein_favourites_admin::get_data($objectType, $guid, $link_for_anonymous);
+
+        $bury = array
+        (
+            'icon' => MIDCOM_STATIC_URL . '/net.nemein.favourites/not-buried.png',
+            'title' => $l10n->get('bury'),
+            'url' => "{$url_prefix}?net_nemein_favourites_execute=bury&net_nemein_favourites_execute_for={$guid}&net_nemein_favourites_url={$url}/json/bury/{$objectType}/{$guid}/"
+        );
+        $fav = array
+        (
+            'icon' => MIDCOM_STATIC_URL . '/net.nemein.favourites/not-favorite.png',
+            'title' => $l10n->get('add to favourites'),
+            'url' => "{$url_prefix}?net_nemein_favourites_execute=fav&net_nemein_favourites_execute_for={$guid}&net_nemein_favourites_url={$url}/json/fav/{$objectType}/{$guid}/"
+        );
+        
+        if (!$data['can_fav'])
+        {
+            $fav['url'] = null;
+        }
+        
+        if (!$data['can_bury'])
+        {
+            $bury['url'] = null;
+        }        
+
+        if ($data['has_faved'])
+        {
+            // User has already favourited the item
+            $fav['icon'] = MIDCOM_STATIC_URL . '/net.nemein.favourites/favorite.png';
+            $fav['title'] = $l10n->get('favourite');
+            $fav['url'] = null;
+            $bury['url'] = null;
+        }
+        
+        if ($data['has_buried'])
+        {
+            // User has already buried the item
+            $bury['icon'] = MIDCOM_STATIC_URL . '/net.nemein.favourites/bury.png';
+            $bury['title'] = $l10n->get('buried');
+            $bury['url'] = null;
+            $fav['url'] = null;
+        }
+        
+        $fav_button  = "<span class=\"net_nemein_favourites\">";
+        if ($fav['url'])
+        {
+            $fav_button .= "{$data['favs']} <a href=\"{$fav['url']}\" class=\"net_nemein_favourites_create\"><img src=\"{$fav['icon']}\" style=\"border: none;\" alt=\"{$fav['title']}\" title=\"{$fav['title']}\" /></a>";
+        }
+        else
+        {
+            $fav_button .= "{$data['favs']} <img src=\"{$fav['icon']}\" style=\"border: none;\" alt=\"{$fav['title']}\" title=\"{$fav['title']}\" />";
+        }
+        if ($bury['url'])
+        {
+            $fav_button .= "{$data['buries']} <a href=\"{$bury['url']}\" class=\"net_nemein_favourites_create\"><img src=\"{$bury['icon']}\" style=\"border: none;\" alt=\"{$bury['title']}\" title=\"{$bury['title']}\" /></a>";
+        }
+        else
+        {
+            $fav_button .= "{$data['buries']} <img src=\"{$bury['icon']}\" style=\"border: none;\" alt=\"{$bury['title']}\" title=\"{$bury['title']}\" />";
+        }
+        $fav_button .= "</span>";
+        return $fav_button;
+    }
+    
     function get_ajax_headers($js_options='', $element_path='.net_nemein_favourites')
     {
         $html = "<script type=\"text/javascript\" src=\"" . MIDCOM_STATIC_URL . "/jQuery/jquery.metadata.js\"></script>\n";
