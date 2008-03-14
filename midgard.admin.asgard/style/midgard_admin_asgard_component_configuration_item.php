2d5e57273ef91d472442e7301e06bb04bb198e41
Andreas Flack <flack@contentcontrol-berlin.de>
2008-03-14 16:25:05 +0000

    forward port r15607
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@15611 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/style/midgard_admin_asgard_component_configuration_item.php
+++ midgard.admin.asgard/style/midgard_admin_asgard_component_configuration_item.php
@@ -2,6 +2,13 @@
 <td class="key" colspan="2"><span><?php echo $_MIDCOM->i18n->get_string($data['key'], $data['name']); ?></span></td>
 </tr>
 <tr class="even">
-<td class="global">&(data['global']:h);</td>
-<td class="local">&(data['local']:h);</td>
+<?php if ($data['local'] == '<strong>N/A</strong>')
+{?>
+    <td class="global" colspan="2">&(data['global']:h);</td>
+<?php }
+else
+{?>
+    <td class="global">&(data['global']:h);</td>
+    <td class="local">&(data['local']:h);</td>
+<?php } ?>
 </tr>
