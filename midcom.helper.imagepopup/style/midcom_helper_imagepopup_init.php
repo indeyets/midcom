03047d97fb2e02ea9b7897fc7a8ced2cfdf5a1bc
Andreas Flack <flack@contentcontrol-berlin.de>
2008-03-16 11:53:29 +0000

    fixed incorrect doctype url
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@15642 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.imagepopup/style/midcom_helper_imagepopup_init.php
+++ midcom.helper.imagepopup/style/midcom_helper_imagepopup_init.php
@@ -3,12 +3,12 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-           "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+           "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title><?php echo $_MIDCOM->get_context_data(MIDCOM_CONTEXT_PAGETITLE); ?></title>
-        <?php 
-        echo $_MIDCOM->print_head_elements(); 
+        <?php
+        echo $_MIDCOM->print_head_elements();
         ?>
     </head>
     <body <?php $_MIDCOM->print_jsonload(); ?>>
