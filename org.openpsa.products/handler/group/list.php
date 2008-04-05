9e95aefc22a4e6853b18467ebd6238211231986b
Niels Breet <nbreet@gmail.com>
2008-04-05 10:05:20 +0000

    Forwardport r15951
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@15952 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.products/handler/group/list.php
+++ org.openpsa.products/handler/group/list.php
@@ -458,7 +458,9 @@ class org_openpsa_products_handler_group_list  extends midcom_baseclasses_compon
 
         $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
 
-        if (count($data['groups']) >= 1) //FIXME: Hack
+        if (   count($data['groups']) >= 1
+            && count($data['products']) == 0
+           ) //FIXME: Hack
         {
             if ( $this->_config->get('disable_subgroups_on_frontpage') !== true ){
                 midcom_show_style('group_header');
