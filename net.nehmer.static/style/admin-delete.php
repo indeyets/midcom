9b71af2bdf9e2b159dc014d6bd4d0e189d721116
Andreas Flack <flack@contentcontrol-berlin.de>
2008-04-06 16:22:48 +0000

    use schema description in edit/create/delete titles
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@15964 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nehmer.static/style/admin-delete.php
+++ net.nehmer.static/style/admin-delete.php
@@ -1,9 +1,12 @@
 <?php
 // Available request keys: article, datamanager, edit_url, delete_url, create_urls
 $dn_data= $data['datamanager']->get_content_html();
+
+$desc = $data['datamanager']->schema->description;
+$title = sprintf($data['l10n_midcom']->get('edit %s'), $data['l10n']->get($desc));
 ?>
 
-<h2><?php echo $data['l10n']->get('delete article'); ?>: &(dn_data['title']);</h2>
+<h2>&(title:h);: &(dn_data['title']);</h2>
 
 <form action="" method="post">
   <input type="submit" name="net_nehmer_static_deleteok" value="<?php echo $data['l10n_midcom']->get('delete'); ?> " />
--- net.nehmer.static/style/admin-edit.php
+++ net.nehmer.static/style/admin-edit.php
@@ -1,7 +1,10 @@
 <?php
 // Available request keys: article, controller, edit_url, delete_url, create_urls
+
+$desc = $data['controller']->datamanager->schema->description;
+$title = sprintf($data['l10n_midcom']->get('edit %s'), $data['l10n']->get($desc));
 ?>
 
-<h2><?php echo $data['l10n']->get('edit article'); ?>: <?php echo $data['controller']->datamanager->types['title']->value; ?></h2>
+<h2>&(title:h);: <?php echo $data['controller']->datamanager->types['title']->value; ?></h2>
 
 <?php $data['controller']->display_form (); ?>
