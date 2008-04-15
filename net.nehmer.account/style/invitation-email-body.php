782efbe66602b99b5eff7e5788dd2d569a185cbb
Andreas Flack <flack@contentcontrol-berlin.de>
2008-04-15 11:43:31 +0000

    fix invitation link
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@16088 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nehmer.account/style/invitation-email-body.php
+++ net.nehmer.account/style/invitation-email-body.php
@@ -1,6 +1,6 @@
 <?php
 $hash = $data['hash'];
-$prefix = substr($_MIDCOM->get_host_prefix(), 0, -1) . $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
+$prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
 $register_link = "{$prefix}register_invitation/{$hash}/";
 $message = $data['user_message'];
 ?>
