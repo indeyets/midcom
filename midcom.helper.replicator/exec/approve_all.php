00808fff82d652d4d8bde4a62a6bee3c4d6b255f
Eero af Heurlin <eero.afheurlin@iki.fi>
2008-06-12 14:16:31 +0000

    forward port r16584
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@16585 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.helper.replicator/exec/approve_all.php
+++ midcom.helper.replicator/exec/approve_all.php
@@ -11,8 +11,9 @@ $_MIDCOM->auth->require_admin_user();
     The reflection based system is not done yet, meanwhile look at the following
 scripts
     <ul>
-        <li><a href="approve_topic_tree.php">approve_topic_tree.php</a></li>
-        <li><a href="approve_style_tree.php">approve_style_tree.php</a></li>
-        <li><a href="approve_snippet_tree.php">approve_snippet_tree.php</a></li>
+        <li><a href="approve_topic_tree_reflector.php">approve_topic_tree_reflector.php</a>, forced approval of everything that is child of topic</li>
+        <li><a href="approve_topic_tree_classic.php">approve_topic_tree_classic.php</a>, the classic topic/article approval if not yet approved</li>
+        <li><a href="approve_style_tree.php">approve_style_tree.php</a>, forced approval of styles and style_elements</li>
+        <li><a href="approve_snippet_tree.php">approve_snippet_tree.php</a>, forced approval of snippetdirs and snippets</li>
     </ul>
 </p>
