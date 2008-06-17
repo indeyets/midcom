24b1295f3c3a26ff9b41a3f449db01b16443ec98
Arttu Manninen <arttu@kaktus.cc>
2008-06-17 07:20:50 +0000

    Added missing style elements to head.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@16620 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/handler/object/attachments.php
+++ midgard.admin.asgard/handler/object/attachments.php
@@ -54,10 +54,19 @@ class midgard_admin_asgard_handler_object_attachments extends midcom_baseclasses
             (
                 'rel' => 'stylesheet',
                 'type' => 'text/css',
-                'href' => MIDCOM_STATIC_URL . '/midgard.admin.asgard/attachments/layout.css',
+                'href' => MIDCOM_STATIC_URL . '/midgard.admin.asgard/style-editor.css',
             )
         );
 
+        $_MIDCOM->add_link_head
+        (
+            array
+            (
+                'rel' => 'stylesheet',
+                'type' => 'text/css',
+                'href' => MIDCOM_STATIC_URL . '/midgard.admin.asgard/attachments/layout.css',
+            )
+        );
     }
 
     function _on_initialize()
