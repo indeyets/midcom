2b04ecc11d5e0743dc724f80aba30362611f1a28
Henri Bergius <henri.bergius@iki.fi>
2008-09-03 10:03:41 +0000

    Autoloading support
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17338 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.bannedwords/word.php
+++ net.nemein.bannedwords/word.php
@@ -8,12 +8,6 @@
  */
 class net_nemein_bannedwords_word_dba extends __net_nemein_bannedwords_word_dba
 {
-/*
-    function net_nemein_bannedwords_word_dba()
-    {
-        parent::__net_nemein_bannedwords_word_dba();
-    }
-    */
 }
 
 ?>
