b731605b13f48199bf95d2145bd8cd7acc284d42
Henri Bergius <henri.bergius@iki.fi>
2008-09-03 13:57:49 +0000

    Missing DBA class for an MgdSchema
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17370 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ org.openpsa.documents/link/organization.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package org.openpsa.documents
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * @package org.openpsa.documents
+ */
+class org_openpsa_document_link_organization_dba extends __org_openpsa_document_link_organization_dba
+{
+}
+?>
