654ff74a94485732f9a8a96ac7397c5af5c68071
Henri Bergius <henri.bergius@iki.fi>
2008-09-04 13:58:40 +0000

    Re-enable ACL cache as it works (readonly) in latest nightly
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17397 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.core/midcom/core/privilege.php
+++ midcom.core/midcom/core/privilege.php
@@ -432,8 +432,8 @@ class midcom_core_privilege extends midcom_core_privilege_db
         else
         {
             // FIXME: Re-enable once unserialize works again with MgdSchema objects
-            //$return = $_MIDCOM->cache->memcache->get('ACL', $guid);
-            $return = null;
+            $return = $_MIDCOM->cache->memcache->get('ACL', $guid);
+            //$return = null;
             if (! is_array($return))
             {
                 $return = midcom_core_privilege::_query_all_privileges($guid);
