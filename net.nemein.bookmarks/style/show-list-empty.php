4a033af399f63faa695697144865d9b83564ec11
Piotr Pokora <piotrek.pokora@gmail.com>
2008-09-04 07:53:20 +0000

    Removed: no manifest file for component
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17377 62f49d1c-1c16-0410-9156-d311ce72abac

--- net.nemein.bookmarks/style/show-list-empty.php
+++
@@ -1,1 +0,0 @@
-<p>No bookmarks found.</p>
--- net.nemein.bookmarks/style/show-submit-error.php
+++
@@ -1,4 +0,0 @@
-<h1><?php echo $GLOBALS["view_l10n"]->get("failed to create bookmark!"); ?></h1>
-<b><?php echo $GLOBALS["view_l10n"]->get("invalid username or password"); ?></b><br/>
-<a href="<?php echo $_POST['net_nemein_bookmarks_add_url']; ?>">
-    <?php echo $GLOBALS["view_l10n"]->get("back to page"); ?></a>
