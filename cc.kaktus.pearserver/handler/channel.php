4d61c66c2c43162b55d28561b3ed3643c41ef714
Arttu Manninen <arttu@kaktus.cc>
2008-09-06 06:37:27 +0000

    Removed obsolete component.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17416 62f49d1c-1c16-0410-9156-d311ce72abac

--- cc.kaktus.pearserver/handler/channel.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package cc.kaktus.pearserver
- * @author The Midgard Project, http://www.midgard-project.org
- * @copyright The Midgard Project, http://www.midgard-project.org
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
- */
-
-?>
--- cc.kaktus.pearserver/style/pearserver-configuration.php
+++
@@ -1,4 +0,0 @@
-<h1><?php echo $data['l10n_midcom']->get('component configuration'); ?></h1>
-<?php
-$data['controller']->display_form();
-?>
