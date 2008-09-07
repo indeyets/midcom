15c0d676a92607906292347ea6e2aaf283deca8e
Marcin Soltysiak <olga@olga.pl>
2008-09-07 11:31:56 +0000

    Major fixes towards Ragna... that difficult word :)
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17441 62f49d1c-1c16-0410-9156-d311ce72abac

--- pl.olga.mnogosearch/style/result-item.php
+++ pl.olga.mnogosearch/style/result-item.php
@@ -1,6 +1,4 @@
 <?php
-$data =& $_MIDCOM->get_custom_context_data('request_data');
-$prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX,0);
 $doc = $data['doc'];
 ?>
   <DT><b>&(doc['ndoc']);.</b><a href="&(doc['url']);"><b>&(doc['title']:h);</b></a> [<b>&(doc['rating']);</b>]</dt>
