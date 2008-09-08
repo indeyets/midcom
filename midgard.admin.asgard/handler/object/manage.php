d7ffc198c9b7f8df3c7a6ad8587770704d207185
Henri Bergius <henri.bergius@iki.fi>
2008-09-08 15:47:43 +0000

    Show component icon in the selection lists to make finding the right component easier visually
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17457 62f49d1c-1c16-0410-9156-d311ce72abac

--- midgard.admin.asgard/handler/object/manage.php
+++ midgard.admin.asgard/handler/object/manage.php
@@ -437,7 +437,7 @@ class midgard_admin_asgard_handler_object_manage extends midcom_baseclasses_comp
                                     'options' => $components,
                                     'allow_other' => true,
                                 ),
-                                'widget'      => 'select',
+                                'widget'      => 'selectcomponent',
                             )
                         );
                         break;
--- midgard.admin.asgard/plugin.php
+++ midgard.admin.asgard/plugin.php
@@ -782,6 +782,11 @@ class midgard_admin_asgard_plugin extends midcom_baseclasses_components_handler
                             break;
                         default:
                             $midcom_dba_classname = $_MIDCOM->dbclassloader->get_midcom_class_name_for_legacy_midgard_class($type);
+                            if (!$midcom_dba_classname)
+                            {
+                                $display_button = false;
+                                break;
+                            }
                             $component = $_MIDCOM->dbclassloader->_mgdschema_class_handler[$midcom_dba_classname];
                             if ($component != $object->component)
                             {
