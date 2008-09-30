indeyets/midcom

commit 7dae48c20523f15544d84fb9d36412065d450d67

Changed jQuery shorthand $j to full jQuery for all-around compatibility.
git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17339 62f49d1c-1c16-0410-9156-d311ce72abac

midcom.helper.datamanager2/widget/sortable.php
@@ -214,7 +214,7 @@ class midcom_helper_datamanager2_widget_sortable extends midcom_helper_datamanag
         {
             $html = "<script type=\"text/javascript\">\n";
             $html .= "    // <![CDATA[\n";
-            $html .= "        \$j('#{$this->name}_sortable').create_sortable();\n";
+            $html .= "        \jQuery('#{$this->name}_sortable').create_sortable();\n";
             $html .= "    // ]]>\n";
             $html .= "</script>\n";
 

midgard.admin.asgard/handler/object/rcs.php
@@ -551,9 +551,9 @@ class midgard_admin_asgard_handler_object_rcs extends midcom_baseclasses_compone
 
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL . '/midgard.admin.asgard/rcs.js');
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL . '/jQuery/jquery.tablesorter.pack.js');
-        $_MIDCOM->add_jscript("\$j(document).ready(function()
+        $_MIDCOM->add_jscript("\jQuery(document).ready(function()
         {
-            \$j('#midgard_admin_asgard_rcs_version_compare table').tablesorter({
+            \jQuery('#midgard_admin_asgard_rcs_version_compare table').tablesorter({
                 headers:
                 {
                     0: {sorter: false},

midgard.admin.asgard/style/midgard_admin_asgard_object_attachments_header.php
@@ -75,7 +75,7 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
             echo "</form>\n";
             echo "<script type=\"text/javascript\">\n";
             echo "// <![CDATA[\n";
-            echo "\$j('#attachment_table').tablesorter(\n";
+            echo "\jQuery('#attachment_table').tablesorter(\n";
             echo "  {\n";
             echo "      widgets: ['zebra'],\n";
             echo "      sortList: [[0,0]]\n";

midgard.admin.asgard/style/midgard_admin_asgard_trash_type.php
@@ -113,10 +113,10 @@ if ($data['trash'])
     echo "            <tr>\n";
     echo "            <td colspan=\"5\">\n";
     echo "                <label for=\"select_all\">\n";
-    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"\$j(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"\jQuery(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "                <label for=\"invert_selection\">\n";
-    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"\$j(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"\jQuery(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "            </td>\n";
     echo "        </tr>\n";
@@ -139,7 +139,7 @@ if ($data['trash'])
     echo "</form>\n";
     echo "<script type=\"text/javascript\">\n";
     echo "// <![CDATA[\n";
-    echo "\$j('#batch_process').tablesorter(\n";
+    echo "\jQuery('#batch_process').tablesorter(\n";
     echo "  {\n";
     echo "      widgets: ['zebra'],\n";
     echo "      sortList: [[1,0]]\n";

midgard.admin.asgard/style/midgard_admin_asgard_type.php
@@ -110,7 +110,7 @@ if (isset($data['search_results']))
         echo "</table>\n";
         echo "<script type=\"text/javascript\">\n";
         echo "        // <![CDATA[\n";
-        echo "            \$j('#search_results').tablesorter(\n";
+        echo "            \jQuery('#search_results').tablesorter(\n";
         echo "            {\n ";
         echo "                widgets: ['zebra'],";
         echo "                sortList: [[0,0]]\n";

midgard.admin.asgard/style/midgard_admin_asgard_welcome.php
@@ -139,10 +139,10 @@ if (count($data['revised']) > 0)
     echo "            <tr>\n";
     echo "            <td colspan=\"5\">\n";
     echo "                <label for=\"select_all\">\n";
-    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"\$j(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"\jQuery(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "                <label for=\"invert_selection\">\n";
-    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"\$j(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"\jQuery(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "            </td>\n";
     echo "        </tr>\n";
@@ -198,7 +198,7 @@ if (count($data['revised']) > 0)
     echo "</table>\n";
     echo "<script type=\"text/javascript\">\n";
     echo "        // <![CDATA[\n";
-    echo "            \$j('#batch_process').tablesorter(\n";
+    echo "            \jQuery('#batch_process').tablesorter(\n";
     echo "            {\n ";
     echo "                widgets: ['zebra'],\n";
     echo "                sortList: [[1,0]]\n";
@@ -269,7 +269,7 @@ else
         echo "</table>\n";
         echo "<script type=\"text/javascript\">\n";
         echo "        // <![CDATA[\n";
-        echo "            \$j('#last_visited').tablesorter(\n";
+        echo "            \jQuery('#last_visited').tablesorter(\n";
         echo "            {\n ";
         echo "                widgets: ['zebra'],\n";
         echo "            });\n";

midcom.admin.folder/handler/order.php
@@ -21,7 +21,7 @@ class midcom_admin_folder_handler_order extends midcom_baseclasses_components_ha
      *
      * @access public
      */
-    public function midcom_admin_folder_handler_order ()
+    function midcom_admin_folder_handler_order ()
     {
         parent::midcom_baseclasses_components_handler();
     }
@@ -157,7 +157,9 @@ class midcom_admin_folder_handler_order extends midcom_baseclasses_components_ha
      */
     function _handler_order($handler_id, $args, &$data)
     {
-        // Include Scriptaculous JavaScript libraries to headers
+        // jQuery sorting
+        $_MIDCOM->enable_jquery();
+        
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL.'/jQuery/jquery.dimensions-1.1.2.js');
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL.'/jQuery/jquery.form-1.0.3.js');
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL.'/jQuery/ui/ui.mouse.js');
@@ -350,7 +352,7 @@ class midcom_admin_folder_handler_order extends midcom_baseclasses_components_ha
      * @param int $int    Integer
      * @return string     String filled with leading zeros
      */
-    private function _get_score($int)
+    function _get_score($int)
     {
         $string = (string) $int;
 

midcom.admin.folder/static/jquery-postfix.js
@@ -1,19 +1,19 @@
-$j(document).ready(function()
+jQuery(document).ready(function()
 {
-    $j('#midcom_admin_folder_order_form ul.sortable input').css('display', 'none');
-    $j('#midcom_admin_folder_order_form_sort_type div.form_toolbar').css('display', 'none');
+    jQuery('#midcom_admin_folder_order_form ul.sortable input').css('display', 'none');
+    jQuery('#midcom_admin_folder_order_form_sort_type div.form_toolbar').css('display', 'none');
     
-    $j('#midcom_admin_folder_order_form_sort_type select').change(function()
+    jQuery('#midcom_admin_folder_order_form_sort_type select').change(function()
     {
-        $j('#midcom_admin_folder_order_form_sort_type').submit();
+        jQuery('#midcom_admin_folder_order_form_sort_type').submit();
     });
     
-    $j('#midcom_admin_folder_order_form_sort_type').submit(function()
+    jQuery('#midcom_admin_folder_order_form_sort_type').submit(function()
     {
         var date = new Date();
-        var location = $j('#midcom_admin_folder_order_form_sort_type').attr('action') + '?ajax&time=' + date.getTime();
+        var location = jQuery('#midcom_admin_folder_order_form_sort_type').attr('action') + '?ajax&time=' + date.getTime();
         
-        $j('#midcom_admin_folder_order_form_sort_type').ajaxSubmit
+        jQuery('#midcom_admin_folder_order_form_sort_type').ajaxSubmit
         (
             {
                 url: location,
@@ -24,15 +24,15 @@ $j(document).ready(function()
         return false;
     });
     
-    $j('#midcom_admin_folder_order_form').submit(function()
+    jQuery('#midcom_admin_folder_order_form').submit(function()
     {
-        $j(this).find('ul').each(function(i)
+        jQuery(this).find('ul').each(function(i)
         {
-            var count = $j(this).find('li').size();
+            var count = jQuery(this).find('li').size();
             
-            $j(this).find('li').each(function(i)
+            jQuery(this).find('li').each(function(i)
             {
-                $j(this).find('input').attr('value', i);
+                jQuery(this).find('input').attr('value', i);
             });
         });
     });

commit 7790d70634a411ce2b30cdafe948c957ccec2f53

fix some js errors introduced in r17339
git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17811 62f49d1c-1c16-0410-9156-d311ce72abac

midcom.helper.datamanager2/widget/sortable.php
@@ -214,7 +214,7 @@ class midcom_helper_datamanager2_widget_sortable extends midcom_helper_datamanag
         {
             $html = "<script type=\"text/javascript\">\n";
             $html .= "    // <![CDATA[\n";
-            $html .= "        \jQuery('#{$this->name}_sortable').create_sortable();\n";
+            $html .= "        jQuery('#{$this->name}_sortable').create_sortable();\n";
             $html .= "    // ]]>\n";
             $html .= "</script>\n";
 

midgard.admin.asgard/handler/object/rcs.php
@@ -551,9 +551,9 @@ class midgard_admin_asgard_handler_object_rcs extends midcom_baseclasses_compone
 
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL . '/midgard.admin.asgard/rcs.js');
         $_MIDCOM->add_jsfile(MIDCOM_STATIC_URL . '/jQuery/jquery.tablesorter.pack.js');
-        $_MIDCOM->add_jscript("\jQuery(document).ready(function()
+        $_MIDCOM->add_jscript("jQuery(document).ready(function()
         {
-            \jQuery('#midgard_admin_asgard_rcs_version_compare table').tablesorter({
+            jQuery('#midgard_admin_asgard_rcs_version_compare table').tablesorter({
                 headers:
                 {
                     0: {sorter: false},

midgard.admin.asgard/style/midgard_admin_asgard_object_attachments_header.php
@@ -75,7 +75,7 @@ $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
             echo "</form>\n";
             echo "<script type=\"text/javascript\">\n";
             echo "// <![CDATA[\n";
-            echo "\jQuery('#attachment_table').tablesorter(\n";
+            echo "jQuery('#attachment_table').tablesorter(\n";
             echo "  {\n";
             echo "      widgets: ['zebra'],\n";
             echo "      sortList: [[0,0]]\n";

midgard.admin.asgard/style/midgard_admin_asgard_trash_type.php
@@ -113,10 +113,10 @@ if ($data['trash'])
     echo "            <tr>\n";
     echo "            <td colspan=\"5\">\n";
     echo "                <label for=\"select_all\">\n";
-    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"\jQuery(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"jQuery(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "                <label for=\"invert_selection\">\n";
-    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"\jQuery(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"jQuery(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "            </td>\n";
     echo "        </tr>\n";
@@ -139,7 +139,7 @@ if ($data['trash'])
     echo "</form>\n";
     echo "<script type=\"text/javascript\">\n";
     echo "// <![CDATA[\n";
-    echo "\jQuery('#batch_process').tablesorter(\n";
+    echo "jQuery('#batch_process').tablesorter(\n";
     echo "  {\n";
     echo "      widgets: ['zebra'],\n";
     echo "      sortList: [[1,0]]\n";

midgard.admin.asgard/style/midgard_admin_asgard_type.php
@@ -110,7 +110,7 @@ if (isset($data['search_results']))
         echo "</table>\n";
         echo "<script type=\"text/javascript\">\n";
         echo "        // <![CDATA[\n";
-        echo "            \jQuery('#search_results').tablesorter(\n";
+        echo "            jQuery('#search_results').tablesorter(\n";
         echo "            {\n ";
         echo "                widgets: ['zebra'],";
         echo "                sortList: [[0,0]]\n";

midgard.admin.asgard/style/midgard_admin_asgard_welcome.php
@@ -139,10 +139,10 @@ if (count($data['revised']) > 0)
     echo "            <tr>\n";
     echo "            <td colspan=\"5\">\n";
     echo "                <label for=\"select_all\">\n";
-    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"\jQuery(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"jQuery(this).check_all('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('select all', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "                <label for=\"invert_selection\">\n";
-    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"\jQuery(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
+    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"jQuery(this).invert_selection('#batch_process tbody');\" />" . $_MIDCOM->i18n->get_string('invert selection', 'midgard.admin.asgard');
     echo "                </label>\n";
     echo "            </td>\n";
     echo "        </tr>\n";
@@ -198,7 +198,7 @@ if (count($data['revised']) > 0)
     echo "</table>\n";
     echo "<script type=\"text/javascript\">\n";
     echo "        // <![CDATA[\n";
-    echo "            \jQuery('#batch_process').tablesorter(\n";
+    echo "            jQuery('#batch_process').tablesorter(\n";
     echo "            {\n ";
     echo "                widgets: ['zebra'],\n";
     echo "                sortList: [[1,0]]\n";
@@ -269,7 +269,7 @@ else
         echo "</table>\n";
         echo "<script type=\"text/javascript\">\n";
         echo "        // <![CDATA[\n";
-        echo "            \jQuery('#last_visited').tablesorter(\n";
+        echo "            jQuery('#last_visited').tablesorter(\n";
         echo "            {\n ";
         echo "                widgets: ['zebra'],\n";
         echo "            });\n";