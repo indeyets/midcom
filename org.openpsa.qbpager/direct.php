indeyets/midcom

commit 0eac1b80d94ceb1116bed7fb5d14136a4e45e862

switch to PHP5-style constructors, part 5
git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17354 62f49d1c-1c16-0410-9156-d311ce72abac

org.openpsa.qbpager/direct.php
@@ -11,7 +11,7 @@ class org_openpsa_qbpager_direct extends org_openpsa_qbpager
 {
     function __construct($classname, $pager_id)
     {
-        parent::midcom_baseclasses_components_purecode();
+        parent::__construct();
         $this->_component = 'org.openpsa.qbpager';
 
         $this->_limit =& $this->results_per_page;

commit c0cb26a29a8a47851fe2ef53e3933fa34e665e42

skip directly to the baseclass constructor as intended, was broken in r17354, fixes #304
git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17807 62f49d1c-1c16-0410-9156-d311ce72abac

org.openpsa.qbpager/direct.php
@@ -11,9 +11,8 @@ class org_openpsa_qbpager_direct extends org_openpsa_qbpager
 {
     function __construct($classname, $pager_id)
     {
-        parent::__construct();
         $this->_component = 'org.openpsa.qbpager';
-
+        midcom_baseclasses_components_purecode::__construct();
         $this->_limit =& $this->results_per_page;
         $this->_pager_id = $pager_id;
         $this->_midcom_qb = new midgard_query_builder($classname);