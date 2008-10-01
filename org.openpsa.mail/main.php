adbbf3f9bf821ef7d4e6773843e001a127283a95
Henri Bergius <henri.bergius@iki.fi>
2008-10-01 17:33:46 +0000

    It seems this utility function is being used in many places
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17842 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.mail/main.php
+++ org.openpsa.mail/main.php
@@ -1214,4 +1214,16 @@ EOF;
     }
 
 }
+
+if (!function_exists('sprint_r')) 
+{
+    function sprint_r($var) 
+    {
+        ob_start();
+        print_r($var);
+        $ret = ob_get_contents();
+        ob_end_clean();
+        return $ret;
+    }
+}
 ?>
