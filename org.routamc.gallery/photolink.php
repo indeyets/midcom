2526c552854f20839899424bf34e75ca9c26acaa
Eero af Heurlin <eero.afheurlin@iki.fi>
2008-10-06 10:16:30 +0000

    forward port the parent resolve part of r17956
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@17957 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.routamc.gallery/photolink.php
+++ org.routamc.gallery/photolink.php
@@ -20,6 +20,22 @@ class org_routamc_gallery_photolink_dba extends __org_routamc_gallery_photolink_
         return parent::__construct($id);
     }
 
+    function get_parent_guid_uncached()
+    {
+        if ($this->node != 0)
+        {
+            $parent = new midcom_db_topic($this->node);
+            return $parent->guid;
+        }
+        else
+        {
+            debug_push_class(__CLASS__, __FUNCTION__);
+            debug_add("No node defined for this photolink", MIDCOM_LOG_DEBUG);
+            debug_pop();
+            return null;
+        }
+    }
+
     function _on_creating()
     {
         if (   !$this->photo
