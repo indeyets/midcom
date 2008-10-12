f29a89f9e2fbcd0d61dc3ef75bbfe339ce728989
Andreas Flack <flack@contentcontrol-berlin.de>
2008-10-12 12:47:48 +0000

    show sidevbar also in creation view
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@18142 62f49d1c-1c16-0410-9156-d311ce72abac

--- org.openpsa.documents/style/show-directory-new.php
+++ org.openpsa.documents/style/show-directory-new.php
@@ -4,3 +4,7 @@
 <div class="main">
     <?php $data['controller']->display_form(); ?>
 </div>
+<div class="sidebar">
+    <?php midcom_show_style("show-search-form-simple"); ?>
+    <?php midcom_show_style("show-directory-navigation"); ?>
+</div>
