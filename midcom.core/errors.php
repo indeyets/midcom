02119e7ac2a31eea211366feba2d0dc7061519e6
Henri Bergius <henri.bergius@iki.fi>
2008-10-15 11:34:24 +0000

    Catch errors that happen before MidCOM is initialized
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@18232 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom.core/errors.php
+++ midcom.core/errors.php
@@ -1,18 +1,33 @@
 <?php
 /**
+ * @package midcom
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @version $Id$
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
  * midcom_exception_handler
  * 
  * Class for intercepting PHP errors and unhandled exceptions. Each fault is caught
  * and converten into Exception handled by $_MIDCOM->generate_error() with 
  * code 500 thus can be customized and make user friendly.
  *
+ * @package midcom
  */
-
 class midcom_exception_handler 
 {  
     public static function print_exception(Exception $e)
     {
-        $_MIDCOM->generate_error(MIDCOM_ERRCRIT,$e->getMessage());
+        if (!isset($_MIDCOM))
+        {
+            header('HTTP/1.0 500 Server Error');
+            echo "<h1>HTTP/1.0 500 Server Error</h1>\n";
+            echo '<p>' . $e->getMessage() . "</p>\n";
+            die();
+        }
+        $_MIDCOM->generate_error(MIDCOM_ERRCRIT, $e->getMessage());
     }
   
     public static function handle_exception(Exception $e)
@@ -33,6 +48,6 @@ class midcom_exception_handler
     }
 }
 
-set_error_handler(array("midcom_exception_handler", "handle_error"), E_ALL & ~E_NOTICE | E_WARNING);
-set_exception_handler(array("midcom_exception_handler", "handle_exception"));
+set_error_handler(array('midcom_exception_handler', 'handle_error'), E_ALL & ~E_NOTICE | E_WARNING);
+set_exception_handler(array('midcom_exception_handler', 'handle_exception'));
 ?>
