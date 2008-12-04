1966f38e272245e68ab0892cb2552ad585114544
Henri Bergius <henri.bergius@iki.fi>
2008-12-04 23:24:46 +0000

    Remove $id keywords, because git does not have support for them
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19293 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/exception_handler.php
+++ midcom_core/exception_handler.php
@@ -2,7 +2,6 @@
 /**
  * @package midcom_core
  * @author The Midgard Project, http://www.midgard-project.org
- * @version $Id:application.php 3765 2006-07-31 08:51:39 +0000 (Mon, 31 Jul 2006) tarjei $
  * @copyright The Midgard Project, http://www.midgard-project.org
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
