cc3115f070f32feb5c761e983060d1039613ff11
Henri Bergius <henri.bergius@iki.fi>
2008-12-04 22:57:02 +0000

    Renaming to underscore
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19283 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom_core/templates/ROOT-edit.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The default HTML5 layout template for Midgard
+ *
+ * @todo convert to XHTML5 as soon as MidCOM 3 javascripts are compatible with it
+ * @package midcom_core
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title tal:content="page/title">Midgard CMS</title>
+        <span tal:replace="php: MIDCOM.head.print_elements()" />
+        <link rel="stylesheet" type="text/css" href="/midcom-static/midcom_core/midgard/screen.css" media="screen,projection,tv" />
+        <link rel="stylesheet" type="text/css" href="/midcom-static/midcom_core/midgard/content.css" media="all" />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <link rel="shortcut icon" href="/midcom-static/midcom_core/midgard/midgard.ico" type="image/vnd.microsoft.icon" />
+    </head>
+    <body>
+        <div id="container">
+            <header>
+                <div class="grouplogo">
+                    <a href="/"><img src="/midcom-static/midcom_core/midgard/midgard.gif" alt="Midgard" width="135" height="138" /></a>
+                </div>
+            </header>
+            <section id="content">
+                <!-- beginning of content-text -->
+                <div id="content-text">
+                    <(content)>
+                </div>
+            </section>
+        </div>
+        <footer>
+             <a href="http://www.midgard-project.org/" rel="powered">Midgard CMS</a> power since 1999. 
+             <a href="http://www.gnu.org/licenses/lgpl.html" rel="license">Free software</a>.
+        </footer>
+        <span tal:condition="show_toolbar" tal:replace="php: MIDCOM.toolbar.render()" />
+        <span tal:condition="uimessages" tal:replace="structure uimessages" />
+    </body>
+</html>
