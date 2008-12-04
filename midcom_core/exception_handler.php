d506f5b35cd9521519973c1543f04fa87b1ce4e0
Henri Bergius <henri.bergius@iki.fi>
2008-12-04 23:16:58 +0000

    Initial exception handler
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19289 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom_core/exception_handler.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package midcom_core
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @version $Id:application.php 3765 2006-07-31 08:51:39 +0000 (Mon, 31 Jul 2006) tarjei $
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * MidCOM 3 exception handler
+ *
+ * @package midcom_core
+ */
+class midcom_core_exception_handler()
+{
+    function handle($exception)
+    {
+        // TODO: Different HTTP error codes for different Exceptions
+        $http_code = 500;
+        $message = $exception->getMessage();
+    
+        if (headers_sent())
+        {
+            die("Unexpected Error, this should display an HTTP {$http_code} - {$message}");
+        }
+        
+        switch ($http_code)
+        {
+            case 200:
+                $header = 'HTTP/1.0 200 OK';
+                break;
+            case 500:
+            default:
+                $header = "HTTP/1.0 500 Server Error";
+                break;
+        }
+        
+        header($header);
+        header('Content-Type: text/html');
+
+        // TODO: Templating
+        echo "<html><h1>{$header}</h1><p>{$message}</p>";
+    }
+}
+
+set_exception_handler(array('midcom_core_exception_handler', 'handle'));
+?>
