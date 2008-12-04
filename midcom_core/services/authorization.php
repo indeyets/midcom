2e2c2f4f6b9110317540445f9a8e3ff48dfa29c9
Henri Bergius <henri.bergius@iki.fi>
2008-12-04 23:01:59 +0000

    Simple authorization interface and implementation
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19285 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom_core/services/authorization.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package midcom_core
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @version $Id:application.php 3765 2006-07-31 08:51:39 +0000 (Mon, 31 Jul 2006) tarjei $
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * Authorization interface for MidCOM 3
+ *
+ * @package midcom
+ */
+interface midcom_core_services_authorization
+{
+    /**
+     * Checks whether a user has a certain privilege on the given content object.
+     * Works on the currently authenticated user by default, but can take another
+     * user as an optional argument.
+     *
+     * @param string $privilege The privilege to check for
+     * @param MidgardObject &$content_object A Midgard Content Object
+     * @param midcom_core_user $user The user against which to check the privilege, defaults to the currently authenticated user.
+     *     You may specify "EVERYONE" instead of an object to check what an anonymous user can do.
+     * @return boolean true if the privilege has been granted, false otherwise.
+     */
+    public function can_do($privilege, $object, $user = null);
+}
+?>
