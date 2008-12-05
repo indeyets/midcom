1ba5f9b35867e20b47c608a9c0f46fffc0f16277
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 08:59:20 +0000

    Some RSS tweaks
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19412 62f49d1c-1c16-0410-9156-d311ce72abac

--- net_nemein_news/templates/nnn-show-rss.php
+++ net_nemein_news/templates/nnn-show-rss.php
@@ -5,10 +5,10 @@
         <generator>Midgard CMS</generator>
         <item tal:repeat="article net_nemein_news/news">
             <title tal:content="article/title">Star City</title>
-            <link>http://liftoff.msfc.nasa.gov/news/2003/news-starcity.asp</link>
-            <description tal:content="article/content"><![CDATA[How do Americans get ready to work with Russians aboard the International Space Station? They take a crash course in cultu
-re, language and protocol at Russia's &lt;a href="http://howe.iki.rssi.ru/GCTC/gctc_e.htm"&gt;Star City&lt;/a&gt;.]]></description>
-            <pubDate tal:content="article/metadata/published">Tue, 03 Jun 2003 09:39:21 GMT</pubDate>
+            <link tal:content="article/url">http://liftoff.msfc.nasa.gov/news/2003/news-starcity.asp</link>
+            <description tal:content="structure article/content">How do Americans get ready to work with Russians aboard the International Space Station? They take a crash course in cultu
+re, language and protocol at Russia's &lt;a href="http://howe.iki.rssi.ru/GCTC/gctc_e.htm"&gt;Star City&lt;/a&gt;.</description>
+            <pubDate tal:content="midcomDateRfc: article/metadata/published">Tue, 03 Jun 2003 09:39:21 GMT</pubDate>
             <guid tal:content="article/guid">http://liftoff.msfc.nasa.gov/2003/06/03.html#item573</guid>
         </item>
     </channel>
