20ed1b621772f6ba54475e81b573e523e45afac5
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 06:42:47 +0000

    Make sure the URLs not having query string have trailing slash or some extension in the "filename".
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19345 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/framework.php
+++ midcom_core/framework.php
@@ -12,6 +12,23 @@ if (!defined('MIDCOM_ROOT'))
     define('MIDCOM_ROOT', dirname(dirname(__FILE__) . '../'));
 }
 
+/**
+ * Make sure the URLs not having query string (or midcom-xxx- -method signature)
+ * have trailing slash or some extension in the "filename".
+ *
+ * This makes life much, much better when making static copies for whatever reason
+ */
+if (   !preg_match('%\?|/$|midcom-.+-|/.+\..+$%', $_SERVER['REQUEST_URI'])
+    && (   !isset($_POST)
+        || empty($_POST))
+    )
+{
+    header('HTTP/1.0 301 Moved Permanently');
+    header("Location: {$_SERVER['REQUEST_URI']}/");
+    echo "301: new location <a href='{$_SERVER['REQUEST_URI']}/'>{$_SERVER['REQUEST_URI']}/</a>";
+    exit();
+}
+
 // Load the exception handler
 require(MIDCOM_ROOT . '/midcom_core/exceptionhandler.php');
 
