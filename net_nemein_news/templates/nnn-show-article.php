05b9e8c0693abeb6b05c74a188fb7b4cf48e1b20
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 07:00:24 +0000

    Initial hAtom microformatting
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19353 62f49d1c-1c16-0410-9156-d311ce72abac

--- net_nemein_news/templates/nnn-show-article.php
+++ net_nemein_news/templates/nnn-show-article.php
@@ -1,3 +1,9 @@
-<h1 tal:content="net_nemein_news/article/title">Headline</h1>
+<div class="hentry">
+    <h1 tal:content="net_nemein_news/article/title" class="entry-title">Headline</h1>
 
-<div tal:content="structure net_nemein_news/article/content">Content</div>
+    <div tal:content="net_nemein_news/article/metadata/published" class="published">2007-08-01</div>
+
+    <div tal:content="structure net_nemein_news/article/content" class="entry-content">
+        Content
+    </div>
+</div>
