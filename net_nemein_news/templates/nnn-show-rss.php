c79f6eaf5aa4dd7bb7054f0d36465b30a3bdc97e
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 00:06:53 +0000

    Initial routes, RSS enabled
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19319 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ net_nemein_news/templates/nnn-show-rss.php
@@ -0,0 +1,15 @@
+<?xml version="1.0"?>
+<rss version="2.0">
+   <channel>
+        <title tal:replace="page/title">News area</title>
+        <generator>Midgard CMS</generator>
+        <item tal:repeat="article net_nemein_news/news">
+            <title tal:content="article/title">Star City</title>
+            <link>http://liftoff.msfc.nasa.gov/news/2003/news-starcity.asp</link>
+            <description tal:content="article/content"><![CDATA[How do Americans get ready to work with Russians aboard the International Space Station? They take a crash course in cultu
+re, language and protocol at Russia's &lt;a href="http://howe.iki.rssi.ru/GCTC/gctc_e.htm"&gt;Star City&lt;/a&gt;.]]></description>
+            <pubDate tal:content="article/metadata/published">Tue, 03 Jun 2003 09:39:21 GMT</pubDate>
+            <guid tal:content="article/guid">http://liftoff.msfc.nasa.gov/2003/06/03.html#item573</guid>
+        </item>
+    </channel>
+</rss>
