0a422f2386a035921cfd98b0b2304fb8c0945869
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 10:05:25 +0000

    Started adding test component for jsmidcom
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19450 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ net_nemein_news/static/component.js
@@ -0,0 +1,26 @@
+/**
+ * @package midcom_core
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+(function($){
+    
+    $.net = $.net || {};
+    $.net.nemein = $.net.nemein || {};
+    
+    $.net.nemein.news = {
+        config: {
+            prefix: ''
+        }
+    };
+    $.extend($.net.nemein.news, {
+        init: function(options) {
+            $.net.nemein.news.options = $.midcom.services.configuration.merge($.net.nemein.news.config, config);
+        }
+    });
+    
+    $.midcom.register_component('net.nemein.news', $.net.nemein.news);
+    
+})(jQuery);
