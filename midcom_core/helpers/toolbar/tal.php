87a253a0e989e3f4440c4fab9f81eb60f127bf1a
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 10:02:23 +0000

    Toolbar now gets the TAL macros from style element
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19448 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/helpers/toolbar/tal.php
+++ midcom_core/helpers/toolbar/tal.php
@@ -21,73 +21,12 @@ class midcom_core_helpers_toolbar_tal extends midcom_core_helpers_toolbar
         $html .= "    <img src=\"".MIDCOM_STATIC_URL."/midcom_core/services/toolbars/midgard-logo.png\" width=\"16\" height=\"16\"/ alt=\"Midgard\">\n";
         $html .= "</a>\n";
         
-        $this->template = <<<EOS
-        <div class="{$this->css_class}" {$this->holder_attributes}>
-            <span tal:condition="toolbar/has_logos" tal:omit-tag="">
-                <div class="{$this->css_class}_logos">
-                    <span tal:repeat="logo toolbar/logos" metal:use-macro="midcom_helper_toolbar_logo" />
-                </div>
-            </span>
-            <div class="{$this->css_class}_items">
-                <span tal:repeat="section toolbar/sections" metal:use-macro="midcom_helper_toolbar_section" />
-            </div>
-            <div class="{$this->css_class}_dragbar"></div>
-        </div>
-
-        <div tal:comment="toolbar logo macro"
-             metal:define-macro="midcom_helper_toolbar_logo" >
+        $data = array(
+            'css_class' => $this->css_class,
+            'holder_attributes' => $this->holder_attributes,
+        );
         
-             <a href="#url" title="title"
-                tal:attributes="href logo/url; title logo/title">
-                 <img src="#src" alt="title"
-                      tal:attributes="src logo/path; alt logo/title" />
-
-             </a>
-
-        </div>
-
-        <div class="\${section/css_class} {$this->css_class}_section"
-             tal:comment="toolbar section macro"
-             metal:define-macro="midcom_helper_toolbar_section" >
-        
-            <span class="\${section/css_class}_title {$this->css_class}_section_title"
-                  tal:content="section/title">Section title here</span>
-            <ul class="\${section/css_class}_items {$this->css_class}_section_items">
-                <span tal:repeat="item section/items"
-                      tal:define="section section"
-                      metal:use-macro="midcom_helper_toolbar_section_item" />
-
-            </ul>
-
-        </div>
-
-        <div tal:comment="toolbar section item macro"
-             tal:omit-tag=""
-             metal:define-macro="midcom_helper_toolbar_section_item" >
-             
-            <li class="\${item/css_class}"
-                tal:condition="item/enabled">
-
-            <span tal:condition="not: item/is_post" tal:omit-tag="">
-                <a href="\${item/url}" title="\${item/label}" class="\${section/css_class}_item_link" accesskey="\${item/accesskey}" >
-                    <span tal:condition="item/icon" tal:omit-tag="">
-
-                    <img src="\${item/iconurl}" alt="\${item/label}" />
-                    </span>
-
-                    &nbsp;<span tal:content="item/htmllabel" class="\${section/css_class}_item_label"></span>
-                </a>
-            </span>
-            <span tal:condition="item/is_post" tal:omit-tag="">
-                <form>
-                Form item
-                </form>
-            </span>
-
-            </li>
-            
-        </div>
-EOS;
+        $this->template = $_MIDCOM->templating->get_template_content('midcom_core', 'midcom_helpers_toolbar_tal', &$data);
     }
     
     public function render(&$toolbar)
---
+++ midcom_core/templates/midcom_helpers_toolbar_tal.php
@@ -0,0 +1,74 @@
+<?php
+$holder_attrs = $data['holder_attributes'];
+$css_class = $data['css_class'];
+?>
+<div class="<?php echo $css_class; ?>" <?php echo $holder_attrs; ?>>
+    <span tal:condition="toolbar/has_logos" tal:omit-tag="">
+
+    <div class="<?php echo $css_class; ?>_logos">
+        <span tal:repeat="logo toolbar/logos" metal:use-macro="midcom_helper_toolbar_logo" />
+    </div>
+    </span>
+    
+    <div class="<?php echo $css_class; ?>_items">
+        <span tal:repeat="section toolbar/sections" metal:use-macro="midcom_helper_toolbar_section" />        
+    </div>
+    <div class="<?php echo $css_class; ?>_dragbar"></div>
+</div>
+
+<div tal:comment="toolbar logo macro"
+     tal:omit-tag=""
+     metal:define-macro="midcom_helper_toolbar_logo" >
+
+        <a href="#url" title="title"
+           tal:attributes="href logo/url; title logo/title">
+            <img src="#src" alt="title"
+                 tal:attributes="src logo/path; alt logo/title" />
+
+        </a>
+
+</div>
+
+<div tal:comment="toolbar section macro"
+     tal:omit-tag=""
+     metal:define-macro="midcom_helper_toolbar_section" >
+     
+         <div class="${section/css_class} <?php echo $css_class; ?>_section">
+            <span class="${section/css_class}_title <?php echo $css_class; ?>_section_title"
+                  tal:content="section/title">Section title here</span>
+
+            <ul class="${section/css_class}_items <?php echo $css_class; ?>_section_items">
+                <span tal:repeat="item section/items"
+                      tal:define="section section"
+                      metal:use-macro="midcom_helper_toolbar_section_item" />
+
+            </ul>
+        </div>
+</div>
+
+<div tal:comment="toolbar section item macro"
+     tal:omit-tag=""
+     metal:define-macro="midcom_helper_toolbar_section_item" >
+     
+                <li class="${item/css_class}"
+                    tal:condition="item/enabled">
+
+                <span tal:condition="not: item/is_post" tal:omit-tag="">
+                    <a href="${item/url}" title="${item/label}" class="${section/css_class}_item_link" accesskey="${item/accesskey}" >
+                        <span tal:condition="item/icon" tal:omit-tag="">
+
+                        <img src="${item/iconurl}" alt="${item/label}" />
+                        </span>
+
+                        &nbsp;<span tal:content="item/htmllabel" class="${section/css_class}_item_label"></span>
+                    </a>
+                </span>
+                <span tal:condition="item/is_post" tal:omit-tag="">
+                    <form>
+                    Form item
+                    </form>
+                </span>
+
+                </li>
+    
+</div>
