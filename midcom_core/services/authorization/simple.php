a29eeb68c250f6e704a73efdebd0bb969fc8d848
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 08:37:31 +0000

    Added two new methods for privilege validation
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19402 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/services/authorization/simple.php
+++ midcom_core/services/authorization/simple.php
@@ -39,5 +39,55 @@ class midcom_core_services_authorization_simple implements midcom_core_services_
         
         return false;
     }
+    
+    /**
+     * Validates, whether the given user have the privilege assigned to him in general.
+     * Be aware, that this does not take any permissions overridden by content objects
+     * into account. Whenever possible, you should user the can_do() variant of this
+     * call therefore. can_user_do is only of interest in cases where you do not have
+     * any content object available, for example when creating root topics.
+     *
+     * If this is not the case, an Access Denied error is generated, the message
+     * defaulting to the string 'access denied: privilege %s not granted' of the
+     * MidCOM main L10n table.
+     *
+     * The check is always done against the currently authenticated user. If the
+     * check is successful, the function returns silently.
+     *
+     * @param string $privilege The privilege to check for
+     * @param string $message The message to show if the privilege has been denied.
+     * @param string $class Optional parameter to set if the check should take type specific permissions into account. The class must be default constructible.
+     */
+    function require_user_do($privilege, $message = null, $class = null)
+    {
+        if (! $this->can_user_do($privilege, null, $class))
+        {
+            if (is_null($message))
+            {
+                $message = "access denied: privilege {$privilege} not granted";
+            }
+            throw new Exception($message);
+            // This will exit.
+        }
+    }
+    
+    /**
+     * Checks, whether the given user have the privilege assigned to him in general.
+     * Be aware, that this does not take any permissions overridden by content objects
+     * into account. Whenever possible, you should user the can_do() variant of this
+     * call therefore. can_user_do is only of interest in cases where you do not have
+     * any content object available, for example when creating root topics.
+     *
+     * @param string $privilege The privilege to check for
+     * @param midcom_core_user $user The user against which to check the privilege, defaults to the currently authenticated user,
+     *     you may specify 'EVERYONE' here to check what an anonymous user can do.
+     * @param string $class Optional parameter to set if the check should take type specific permissions into account. The class must be default constructible.
+     * @param string $component Component providing the class
+     * @return boolean True if the privilege has been granted, false otherwise.
+     */
+    function can_user_do($privilege, $user = null, $class = null, $component = null)
+    {
+        return $this->can_do();
+    }
 }
 ?>
