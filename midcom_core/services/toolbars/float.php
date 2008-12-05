2713afa3664250c0f71c5d37a99d6aaa359f4028
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 11:43:31 +0000

    Added test compability
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19511 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/services/toolbars/float.php
+++ midcom_core/services/toolbars/float.php
@@ -6,6 +6,12 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
 
+if (MIDCOM_TEST_RUN)
+{
+    $path_parts = pathinfo(__FILE__);
+    require_once("{$path_parts['dirname']}/../toolbars.php");
+}
+
 /**
  * float toolbar
  *
