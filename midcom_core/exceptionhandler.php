4040fcfbe384211624234229a65f10af9369277d
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 00:08:30 +0000

    Add http error codes.
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19320 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/exceptionhandler.php
+++ midcom_core/exceptionhandler.php
@@ -19,28 +19,37 @@ class midcom_core_exceptionhandler
         $http_code = 500;
         $message_type = get_class($exception);
         $message = $exception->getMessage();
-    
+
         if (headers_sent())
         {
             die("Unexpected Error, this should display an HTTP {$http_code} - {$message_type}: {$message}");
         }
-        
+
         switch ($http_code)
         {
             case 200:
                 $header = 'HTTP/1.0 200 OK';
                 break;
+            case 304:
+                $header = 'HTTP/1.0 304 Not Modified';
+                break;
+            case 404:
+                $header = 'HTTP/1.0 404 Not Found';
+                break;
             case 500:
             default:
-                $header = "HTTP/1.0 500 Server Error";
+                $header = 'HTTP/1.0 500 Server Error';
                 break;
         }
-        
-        header($header);
-        header('Content-Type: text/html');
 
-        // TODO: Templating
-        echo "<html><h1>{$header}</h1><p>{$message_type}: {$message}</p>";
+        header($header);
+        if ($http_code != 304)
+        {
+            header('Content-Type: text/html');
+
+            // TODO: Templating
+            echo "<html><h1>{$header}</h1><p>{$message_type}: {$message}</p>";
+        }
     }
 }
 
