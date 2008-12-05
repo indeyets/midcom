2186dfc287fb47453680dd3834e95fad076ed681
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 11:28:26 +0000

    Fixed correct component name to header
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19478 62f49d1c-1c16-0410-9156-d311ce72abac

--- net_nemein_news/static/component.js
+++ net_nemein_news/static/component.js
@@ -1,5 +1,5 @@
 /**
- * @package midcom_core
+ * @package net_nemein_news
  * @author The Midgard Project, http://www.midgard-project.org
  * @copyright The Midgard Project, http://www.midgard-project.org
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
