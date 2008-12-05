7ea679c17227f9818d6c284a263986d3e2926902
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 11:57:00 +0000

    Added toolbar and uimessages to default style
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19566 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/templates/ROOT.php
+++ midcom_core/templates/ROOT.php
@@ -3,6 +3,9 @@
  * @package midcom_core
  */
 ?>
+<?php
+echo '<?' . 'xml version="1.0"' . '?>';
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
@@ -106,5 +109,7 @@
             </div>
 
         </div>
+        <span tal:condition="show_toolbar" tal:replace="php: MIDCOM.toolbar.render()" />
+        <span tal:condition="uimessages" tal:replace="structure uimessages" />
     </body>
 </html>
