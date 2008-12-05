d9268cafd4a3f0b02fc76fe61360b2be5aab126e
Henri Bergius <henri.bergius@iki.fi>
2008-12-05 08:41:02 +0000

    Missing component interface class
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@19404 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom_helper_datamanager/interface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package midcom_helper_datamanager
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * Data management component
+ *
+ * @package midcom_helper_datamanager
+ */
+class midcom_helper_datamanager extends midcom_core_component_baseclass
+{
+}
+?>
