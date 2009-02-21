c7805582b81f5eaf593de4d8527fcba3006351e5
Henri Bergius <henri.bergius@iki.fi>
2009-02-21 22:04:37 +0000

    Support for cache invalidation, refs #851
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@20605 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom_core/controllers/cache.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package midcom_core
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * MidCOM cache management controller
+ *
+ * @package midcom_core
+ */
+class midcom_core_controllers_cache
+{
+    public function __construct($instance)
+    {
+        $this->configuration = $_MIDCOM->configuration;
+    }
+    
+    public function action_invalidate($route_id, &$data, $args)
+    {
+        $_MIDCOM->cache->invalidate_all();
+        header('Location: ' . $_MIDCOM->dispatcher->generate_url('page_show', array()));
+        exit();
+    }
+}
+?>
