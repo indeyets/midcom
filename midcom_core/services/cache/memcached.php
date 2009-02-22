364e0245db3ce6329edd167f09f4b4302176a1b2
Henri Bergius <henri.bergius@iki.fi>
2009-02-22 02:15:01 +0000

    Initial memcached provider
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@20621 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ midcom_core/services/cache/memcached.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package midcom_core
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+include MIDCOM_ROOT . "/midcom_core/services/cache.php";
+
+/**
+ * memcached cache backend.
+ * 
+ * Backend requires Memcache PECL package for PHP, and memcached to be running.
+ *
+ * @package midcom_core
+ */
+class midcom_core_services_cache_memcached extends midcom_core_services_cache_base implements midcom_core_services_cache
+{
+    private $memcache;
+    private $name;
+    
+    public function __construct()
+    {
+        if (!extension_loaded('memcache'))
+        {
+            throw new Exception('memcached cache configured but "Memcache" PHP extension not installed.');
+        }
+
+        $this->memcache = new Memcache();
+        $this->memcache->pconnect('localhost', 11211);
+        
+        $this->name = $_MIDCOM->context->host->name;
+
+        parent::__construct();
+    }
+
+    public function put($module, $identifier, $data)
+    {
+        $this->memcache->set("{$this->name}-{$module}-{$identifier}", $data);
+    }
+    
+    public function get($module, $identifier)
+    {
+        return $this->memcache->get("{$this->name}-{$module}-{$identifier}");
+    }       
+    
+    public function delete($module, $identifier)
+    {
+        return $this->memcache->get("{$this->name}-{$module}-{$identifier}");
+    }
+    
+    public function exists($module, $identifier)
+    {
+        return ($this->get($module, $identifier) !== false);
+    }
+    
+    public function delete_all($module)
+    {
+        $this->memcache->flush();
+    }
+}
+?>
