f2366c8fbfdcf610cfba664037569945162d89bb
Tero Heikkinen <tero.heikkinen@iki.fi>
2009-02-22 11:22:41 +0000

    Fixing manual dispacher to put same context data than normal dispatcher regarding to page refs #851
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@20623 62f49d1c-1c16-0410-9156-d311ce72abac

--- midcom_core/services/dispatcher/manual.php
+++ midcom_core/services/dispatcher/manual.php
@@ -42,13 +42,9 @@ class midcom_core_services_dispatcher_manual implements midcom_core_services_dis
             return;
         }
            
-        $page_data = array();
-        $page_data['guid'] = $this->page->guid;
-        $page_data['title'] = $this->page->title;
-        $page_data['content'] = $this->page->content;
         $_MIDCOM->context->uri = $this->get_page_prefix();        
         $_MIDCOM->context->component = $this->page->component;
-        $_MIDCOM->context->page = $page_data;
+        $_MIDCOM->context->page = $this->page;
         $_MIDCOM->context->prefix = $this->get_page_prefix();
         $_MIDCOM->templating->append_page($this->page->id);
     }
