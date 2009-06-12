f26ed8b6a170f9c76e04c568bfdab9a7bf464411
Henri Bergius <henri.bergius@iki.fi>
2009-06-12 10:40:08 +0000

    Scaffolding the dashboard component (yes, the typo in name is intentional)
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@22469 62f49d1c-1c16-0410-9156-d311ce72abac

---
+++ net_nemein_dasboard/interface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package net_nemein_dasboard
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * Dashboard and toolbar component
+ *
+ * @package net_nemein_dasboard
+ */
+class net_nemein_dasboard extends midcom_core_component_baseclass
+{
+}
+?>
