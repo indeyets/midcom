82c7f2ee1fd6db4bf8b6eba2c4d7bc86b05b3b15
Joonas Bergius <joonas.bergius@gmail.com>
2009-06-16 12:28:44 +0000

    Added cookie support & category downloading
    git-svn-id: https://svn.midgard-project.org/midgard/trunk/midcom@22537 62f49d1c-1c16-0410-9156-d311ce72abac

--- net_nemein_dasboard/static/js/toolbar.js
+++ net_nemein_dasboard/static/js/toolbar.js
@@ -3,6 +3,7 @@ jQuery(function($) {
         var defaults = {
             static_url: '/midcom-static/net_nemein_dasboard',
             toggle_active_class: 'active',
+            json_url: '/mgd:actions/',
         };
         
         var opts = $.extend({}, defaults, options);
@@ -18,17 +19,16 @@ jQuery(function($) {
                 $modal = $.midcom_toolbar_modal;
             });
             
-            $root = $('<div />').attr('id', 'toolbar').append("<div id='toolbar:toggle'></div><ul id='toolbar:tabs'><li><a href='#toolbar:items'>items</a></li></ul><div id='toolbar:actions'><div id='toolbar:items'></div></div>").hide();
+            $root = $('<div />').attr('id', 'toolbar').append("<div id='toolbar:toggle'></div><ul id='toolbar:tabs'><li><a href='#toolbar_items'>items</a></li></ul><div id='toolbar:actions'><div id='toolbar_items'></div></div>").hide();
             $('body').append($root);
             
             $toolbar = $('#toolbar');
             var $toggler = $('#toolbar\\:toggle').fadeTo(0, 0.25);
             $tab_container = $('#toolbar\\:tabs').hide();
-            $tabs = $('#toolbar\\:tabs a');
             $action_container = $("#toolbar\\:actions");
             $actions = $("#toolbar\\:actions > div").hide();
-            $editing = $actions.filter('#toolbar\\:items');
-            
+            $editing = $actions.filter('#toolbar_items');
+            $tabs = $('#toolbar\\:tabs li a');
             $elements = $('[mgd\\:guid]');
 
             var editables = [];
@@ -44,29 +44,68 @@ jQuery(function($) {
                 editables[a++] = '</a>';
             };
             $editing.append(editables.join(''));
-            
+
             $editables = $('a', $editing);
 
-            debug('foo');
-            
             $editables.click(function() {
                 objguid = this.getAttribute('href');
                 $modal({ guid: objguid });
                 return false;
             });
 
-            
+            // At this point, we are interested in all the midgard pages since they can add actions to the toolbar
+            $pages = $("[mgd\\:type^='midgard_page']");
+
+            $.getJSON(opts.json_url+'categories.json',{},
+            function(json){
+                for (key in json.categories)
+                {
+                    $('<li></li>').append( 
+                        $tab = $('<a />').attr("href", "#toolbar_"+json.categories[key]).text(json.categories[key])
+                    ).appendTo($tab_container);
+                    $tabs = $tabs.add($tab);
+                    
+                    $category_actions = $('<div />').attr("id", "toolbar_"+json.categories[key]).hide().appendTo($action_container);
+                    $actions = $actions.add($category_actions);
+                    
+                    for (var i=0; i < $pages.length; i++) {
+                        guid = $pages[i].getAttribute('mgd:guid');
+                        url = opts.json_url+guid+'/'+json.categories[key]+'.json';
+                        $.ajax({
+                            type: 'GET',
+                            url: url,
+                            dataType: 'json',
+                            success: function (json) {
+                                for (action in json.actions)
+                                {
+                                    $('<a />').attr("href", json.actions[action].url).text(json.actions[action].label).css('background-image', "url('/midcom-static/"+json.actions[action].icon+"')").appendTo($category_actions);
+                                }
+                            },
+                            data: {},
+                            async: false
+                        });                        
+                    };
+                }
+            });
+                        
             $toggler.toggle(
                 function() {
                     // TODO: Convert this in to a filter that opens the first tab with a click.
-                    $tab_container.show();
-                    $actions.filter(':first').show();
-                    $toggler.data('active', true).addClass(opts.toggle_class);
+                    last_tab = cookie('midcom_toolbar');
+                    if (last_tab)
+                    {
+                        $tab_container.show().find("li a[href^='"+last_tab+"']").click()
+                    }
+                    else
+                    {
+                        $tab_container.show().find(':first a').click();
+                    }                    
+                    $toggler.data('active', true).addClass(opts.toggle_active_class);
                 },
                 function() {
                     $tab_container.hide();
                     $actions.hide();
-                    $toggler.data('active', false).removeClass(opts.toggle_class);
+                    $toggler.data('active', false).removeClass(opts.toggle_active_class);
                 }
             ).hover(
                 function() {
@@ -84,16 +123,16 @@ jQuery(function($) {
                 }
             );
             
-            $tabs.click(function() {
+            $tabs.live("click", function(event) {
                 // TODO: Save the last clicked tab in to a cookie, possibly on window unload or on every click?
                 hash = this.hash.replace(/:/, "\\:");
                 $actions.hide().filter(hash).show();
+                cookie('midcom_toolbar', hash);
                 $tabs.removeClass('selected');
                 $(this).addClass('selected');
                 return false;
             });
             
-            debug('Go!');
             // Ready!
             $root.show();
         }
@@ -102,6 +141,49 @@ jQuery(function($) {
         function debug (msg)
         {
             console.log('debug: ' + msg);
+        }
+        
+        function cookie(name, value, options)
+        {
+            if (typeof value != 'undefined') { // name and value given, set cookie
+                options = options || {};
+                if (value === null) {
+                    value = '';
+                    options.expires = -1;
+                }
+                var expires = '';
+                if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+                    var date;
+                    if (typeof options.expires == 'number') {
+                        date = new Date();
+                        date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+                    } else {
+                        date = options.expires;
+                    }
+                    expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+                }
+                // CAUTION: Needed to parenthesize options.path and options.domain
+                // in the following expressions, otherwise they evaluate to undefined
+                // in the packed version for some reason...
+                var path = options.path ? '; path=' + (options.path) : '';
+                var domain = options.domain ? '; domain=' + (options.domain) : '';
+                var secure = options.secure ? '; secure' : '';
+                document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+            } else { // only name given, get cookie
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
         }
     };
         
